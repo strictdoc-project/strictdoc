ab987135c70079141280ddb2d3bf81325cfc8b77
Stanislav Pankevich <s.pankevich@gmail.com>
2020-06-24 16:52:50 +0200

    Styles: tweak padding/margin of the table view cells

--- strictdoc/gui/styles.py
+++ strictdoc/gui/styles.py
@@ -6,19 +6,29 @@ QTableView {
     /* do not seem work */
     color: 'green';
     background: 'red';
+    background-color: 'red';
 }
 QTableView::item {
+    /* 
+     * padding and margin are important for edited cell to appear on the same
+     * position 
+     */
+    padding: 0px 0px;
+    margin: -3px;
     /* do not seem work */
     background: 'green';
+    background-color: 'green';
     border: 2px solid 'red';
 }
 QTableView:item:selected {
     /* do not seem work */
     background-color: 'green'; 
-    color: '#FFFFFF'
+    color: '#FFFFFF';
+    border: 2px solid 'red';
 }
 QTableView:item:selected:focus {
     /* do not seem work */
+    border: 2px solid 'red';
     background-color: 'green';
 }
 """

060b7e58dd32d36e3f8e18203ecfea4a18883a8d
Stanislav Pankevich <s.pankevich@gmail.com>
2020-06-24 18:46:57 +0200

    Styles: set * styles to 0px: margins, paddings, borders

--- strictdoc/gui/styles.py
+++ strictdoc/gui/styles.py
@@ -1,34 +1,22 @@
 
 STRICTDOC_QT_STYLES = """
+* {
+    padding: 0px 0px;
+    margin: 0px 0px;
+    border: 0px;
+}
 QTableView {
     border: 1px solid '#cccccc';
     gridline-color: '#f0f0f0';
-    /* do not seem work */
-    color: 'green';
-    background: 'red';
-    background-color: 'red';
+    background-color: '#cccccc';
 }
 QTableView::item {
-    /* 
-     * padding and margin are important for edited cell to appear on the same
-     * position 
-     */
-    padding: 0px 0px;
-    margin: -3px;
-    /* do not seem work */
-    background: 'green';
-    background-color: 'green';
-    border: 2px solid 'red';
+
 }
 QTableView:item:selected {
-    /* do not seem work */
-    background-color: 'green'; 
-    color: '#FFFFFF';
-    border: 2px solid 'red';
+
 }
 QTableView:item:selected:focus {
-    /* do not seem work */
-    border: 2px solid 'red';
-    background-color: 'green';
+
 }
 """

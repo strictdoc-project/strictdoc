4c9ec70fc08aa1d0a14319e78654a0e045512228
Stanislav Pankevich <s.pankevich@gmail.com>
2020-10-27 19:41:02 +0100

    Remove strictdoc's first generation code

--- strictdoc/backend/rst/rst_parser_shared_state.py
+++
@@ -1,9 +0,0 @@
-from collections import OrderedDict
-
-
-class RSTParserSharedState:
-    categories_spec = OrderedDict()
-
-    @classmethod
-    def reset(cls):
-        RSTParserSharedState.categories_spec.clear()

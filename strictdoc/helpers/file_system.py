b16370ef14d9bc536a8c7a556fc44a505325ac18
Stanislav Pankevich <s.pankevich@gmail.com>
2020-10-27 13:40:39 +0100

    export: support both full and relative output paths

--- strictdoc/helpers/file_system.py
+++ strictdoc/helpers/file_system.py
@@ -1,8 +1,10 @@
 import distutils.log
 import distutils.dir_util
+import os
 
 
 def sync_dir(src_dir, dst_dir):
+    assert os.path.isdir(src_dir)
     distutils.log.set_verbosity(distutils.log.DEBUG)
     distutils.dir_util.copy_tree(
         src_dir,

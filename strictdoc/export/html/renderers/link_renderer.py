0a44129fa44f7faa87b88cf75e11c3426dcf2c8a
Stanislav Pankevich <s.pankevich@gmail.com>
2020-11-06 21:51:58 +0100

    export/html: LinkRenderer: use Req.UID when there is no title

--- strictdoc/export/html/renderers/link_renderer.py
+++ strictdoc/export/html/renderers/link_renderer.py
@@ -9,7 +9,11 @@ class LinkRenderer:
         if isinstance(node, Section):
             return self._string_to_link(node.title)
         if isinstance(node, Requirement):
-            return self._string_to_link(node.title)
+            if node.title and len(node.title) > 0:
+                return self._string_to_link(node.title)
+            if node.uid and len(node.uid) > 0:
+                return self._string_to_link(node.uid)
+            return ""
         raise NotImplementedError
 
     @staticmethod

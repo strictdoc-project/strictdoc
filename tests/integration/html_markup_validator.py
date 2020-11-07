a5e2ea87458de0030aeff937121aa1c319dccc84
Stanislav Pankevich <s.pankevich@gmail.com>
2020-11-07 12:57:21 +0100

    tests/integration: HTML markup validator: use all 3 validators

--- tests/integration/html_markup_validator.py
+++ tests/integration/html_markup_validator.py
@@ -19,24 +19,27 @@ if not os.path.isfile(input_file):
 with open(input_file, 'r') as file:
     html_content = file.read()
 
-# html5parser = html5lib.HTMLParser(strict=True)
-# try:
-#     html5parser.parse(html_content)
-# except NotImplementedError as e:
-#     print(e)
-#     exit(1)
-#
-# from tidylib import tidy_document
-# document, errors = tidy_document(html_content, options={
-#     'new-blocklevel-tags': 'main',
-#     'char-encoding': 'utf8',
-#     'input-encoding': 'utf8'
-# })
-#
-# for error in errors.split('\n'):
-#     print(error)
+html5parser = html5lib.HTMLParser(strict=True)
+try:
+    html5parser.parse(html_content)
+except NotImplementedError as e:
+    print(e)
+    exit(1)
+
+from tidylib import tidy_document
+document, errors = tidy_document(html_content, options={
+    'new-blocklevel-tags': 'main',
+    'char-encoding': 'utf8',
+    'input-encoding': 'utf8'
+})
+
+for error in errors.split('\n'):
+    print(error)
 
 from xml.etree import ElementTree as etree
 from io import StringIO
-obj = etree.parse(StringIO(html_content), etree.XMLParser())
-print(obj)
+try:
+    etree.parse(StringIO(html_content), etree.XMLParser())
+except Exception as e:
+    print(e)
+    exit(1)

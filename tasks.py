df1305d3d3f542cb15f42417d9b7ef6d9bab0dd5
Stanislav Pankevich <s.pankevich@gmail.com>
2020-11-08 21:05:23 +0100

    tasks: echo executed commands

--- tasks.py
+++ tasks.py
@@ -8,66 +8,13 @@ def formatted_command(string):
     return re.sub('\\s+', ' ', string).strip()
 
 
-@task
-def sphinx(c):
-    c.run(formatted_command("""
-        python3 strictdoc/cli/main.py export docs --output-dir output/sphinx
-    """))
-
-    c.run(formatted_command("""
-        cp -v output/sphinx/rst/StrictDoc.rst docs/sphinx/source/ &&
-        cp -rv output/sphinx/html/* docs/strictdoc-html/strictdoc-html
-    """))
-
-    c.run(formatted_command("""
-        cd docs/sphinx &&
-            make html latexpdf &&
-            open build/latex/strictdoc.pdf
-    """))
-
-
-@task
-def test_unit(c):
-    cwd = os.getcwd()
-
-    command = formatted_command("""
-        pytest --capture=no
-    """)
-
-    c.run("{}".format(command))
-
-
-@task
-def test_integration(c, focus=None, debug=False):
-    clean(c)
-
-    cwd = os.getcwd()
-
-    strictdoc_exec = 'python \\"{cwd}/strictdoc/cli/main.py\\"'.format(cwd=cwd)
-
-    focus_or_none = '--filter {}'.format(focus) if focus else ''
-    debug_opts = '-vv --show-all' if debug else ''
-
-    command = formatted_command("""
-        lit
-        --param STRICTDOC_EXEC="{strictdoc_exec}"
-        -v
-        {debug_opts}    
-        {focus_or_none}
-        {cwd}/tests/integration
-    """).format(strictdoc_exec=strictdoc_exec,
-                cwd=cwd,
-                debug_opts=debug_opts,
-                focus_or_none=focus_or_none)
-
-    print(command)
-    c.run("{}".format(command))
-
-
-@task
-def test(c):
-    test_unit(c)
-    test_integration(c)
+def run_invoke_cmd(c, cmd):
+    c.run(cmd,
+          env=None,
+          hide=False,
+          warn=False,
+          pty=False,
+          echo=True)
 
 
 @task
@@ -95,5 +42,60 @@ def clean(c):
         """echo {find_result} | xargs rm -rfv""".format(find_result=find_result_stdout)
     )
 
-    c.run("{}".format(echo_command))
+    run_invoke_cmd(c, echo_command)
 
+
+@task
+def sphinx(c):
+    run_invoke_cmd(c, formatted_command("""
+        python3 strictdoc/cli/main.py export docs --output-dir output/sphinx
+    """))
+
+    run_invoke_cmd(c, formatted_command("""
+        cp -v output/sphinx/rst/StrictDoc.rst docs/sphinx/source/ &&
+        cp -rv output/sphinx/html/* docs/strictdoc-html/strictdoc-html
+    """))
+
+    run_invoke_cmd(c, formatted_command("""
+        cd docs/sphinx &&
+            make html latexpdf &&
+            open build/latex/strictdoc.pdf
+    """))
+
+
+@task
+def test_unit(c):
+    command = formatted_command("""
+        pytest --capture=no
+    """)
+
+    run_invoke_cmd(c, command)
+
+
+@task(clean)
+def test_integration(c, focus=None, debug=False):
+    cwd = os.getcwd()
+
+    strictdoc_exec = 'python \\"{cwd}/strictdoc/cli/main.py\\"'.format(cwd=cwd)
+
+    focus_or_none = '--filter {}'.format(focus) if focus else ''
+    debug_opts = '-vv --show-all' if debug else ''
+
+    command = formatted_command("""
+        lit
+        --param STRICTDOC_EXEC="{strictdoc_exec}"
+        -v
+        {debug_opts}    
+        {focus_or_none}
+        {cwd}/tests/integration
+    """).format(strictdoc_exec=strictdoc_exec,
+                cwd=cwd,
+                debug_opts=debug_opts,
+                focus_or_none=focus_or_none)
+
+    run_invoke_cmd(c, command)
+
+
+@task(test_unit, test_integration)
+def test(c):
+    pass

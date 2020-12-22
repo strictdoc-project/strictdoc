dfd5ae5f8dfd6dcc2a74ae1755f669bca352f549
Stanislav Pankevich <s.pankevich@gmail.com>
2020-12-22 20:01:18 +0100

    export/rst: print parent and child links
    Closes #237

--- strictdoc/export/rst/writer.py
+++ strictdoc/export/rst/writer.py
@@ -3,6 +3,7 @@ from enum import Enum
 from strictdoc.backend.dsl.models.requirement import Requirement
 from strictdoc.backend.dsl.models.section import FreeText, Section
 from strictdoc.core.document_iterator import DocumentCachingIterator
+from strictdoc.core.traceability_index import TraceabilityIndex
 
 
 class TAG(Enum):
@@ -12,8 +13,8 @@ class TAG(Enum):
 
 
 class RSTWriter:
-    def __init__(self):
-        pass
+    def __init__(self, index: TraceabilityIndex):
+        self.index = index
 
     def write(self, document, single_document):
         document_iterator = DocumentCachingIterator(document)
@@ -27,78 +28,100 @@ class RSTWriter:
 
         for content_node in document_iterator.all_content():
             if isinstance(content_node, Section):
-                output += self._print_rst_header(content_node.title, content_node.ng_level)
+                output += self._print_rst_header(
+                    content_node.title, content_node.ng_level
+                )
                 for free_text in content_node.free_texts:
                     output += self._print_free_text(free_text)
 
             elif isinstance(content_node, Requirement):
-                output += self._print_rst_header(content_node.title, content_node.ng_level)
                 output += self._print_requirement_fields(content_node)
 
         return output.lstrip()
 
     def _print_rst_header(self, string, level):
         chars = {
-            0: '$',
-            1: '=',
-            2: '-',
-            3: '~',
-            4: '^',
+            0: "$",
+            1: "=",
+            2: "-",
+            3: "~",
+            4: "^",
             5: '"',
-            6: '#',
+            6: "#",
             7: "'",
         }
         header_char = chars[level]
-        output = ''
+        output = ""
         output += string
         output += "\n"
         output += header_char.rjust(len(string), header_char)
-        output += "\n"
-        output += "\n"
+        output += "\n\n"
         return output
 
-    def _print_requirement_fields(self, section_content):
-        output = ''
+    def _print_requirement_fields(self, section_content: Requirement):
+        output = ""
 
         if section_content.uid:
+            output += ".. _{}:".format(section_content.uid)
+            output += "\n\n"
+            output += self._print_rst_header(
+                section_content.title, section_content.ng_level
+            )
             output += "``["
             output += section_content.uid
             output += "]``"
-            output += "\n"
-            output += "\n"
+            output += "\n\n"
+        else:
+            output += self._print_rst_header(
+                section_content.title, section_content.ng_level
+            )
 
         if section_content.statement:
             output += section_content.statement
-            output += "\n"
-            output += "\n"
+            output += "\n\n"
         elif section_content.statement_multiline:
             output += section_content.statement_multiline
-            output += "\n"
-            output += "\n"
+            output += "\n\n"
         else:
             pass  # raise RuntimeError('Statement is missing')
 
         if section_content.body:
             output += "\n"
             output += section_content.body
-            output += "\n"
-            output += "\n"
+            output += "\n\n"
 
         for comment in section_content.comments:
-            output += '**'
+            output += "**"
             output += "Comment:"
-            output += '** '
+            output += "** "
             output += comment.get_comment()
+            output += "\n\n"
+
+        if section_content.references and len(section_content.references) > 0:
+            output += "**Parents:**"
+            output += "\n\n"
+            for reference in section_content.references:
+                output += "- *[{}]* ".format(reference.path)
+                output += ":ref:`{}`".format(reference.path)
+                output += "\n"
             output += "\n"
+
+        if self.index.has_children_requirements(section_content):
+            output += "**Children:**"
+            output += "\n\n"
+            for child_requirement in self.index.get_children_requirements(
+                section_content
+            ):
+                output += "- *[{}]* ".format(child_requirement.uid)
+                output += ":ref:`{}`".format(child_requirement.uid)
+                output += "\n"
             output += "\n"
 
         return output
 
     def _print_free_text(self, free_text):
         assert isinstance(free_text, FreeText)
-        output = ''
+        output = ""
         output += free_text.text
-        output += '\n'
-        output += '\n'
-
+        output += "\n\n"
         return output

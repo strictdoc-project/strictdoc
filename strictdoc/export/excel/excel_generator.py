2ae5d881d08ca4733d478983fc5747a83aa040b5
Stanislav Pankevich <s.pankevich@gmail.com>
2020-12-28 23:05:02 +0100

    export/excel: basic Excel export

---
+++ strictdoc/export/excel/excel_generator.py
@@ -0,0 +1,182 @@
+import os
+from pathlib import Path
+from typing import Dict, List
+
+import xlsxwriter
+from xlsxwriter.workbook import Workbook
+from xlsxwriter.worksheet import Worksheet
+from xlsxwriter.utility import xl_cell_to_rowcol_abs
+
+from strictdoc.backend.dsl.models.document import Document
+from strictdoc.backend.dsl.models.requirement import Requirement
+from strictdoc.core.document_iterator import DocumentCachingIterator
+from strictdoc.core.document_tree import DocumentTree
+
+
+EXCEL_SHEET_NAME = "Requirements"
+MAX_WIDTH = 75
+HEADER_MARGIN = 3
+MAX_WIDTH_KEY = "max_width"
+COLUMN_HEADER_KEY = "header"
+PARENT_COLUMN_HEADER_LABEL = "PARENT"
+EXPORT_COLUMNS = [
+    {
+        "name": "uid",
+        "header": "UID",
+    },
+    # {
+    #     "name": "title",
+    #     "header": "TITLE",
+    # },
+    {
+        "name": "statement",
+        "header": "STATEMENT",
+    },
+    # {
+    #     "name": "rationale",
+    #     "header": "RATIONALE",
+    # },
+    # {
+    #     "name": "body",
+    #     "header": "BODY",
+    # },
+]
+
+
+class ExcelGenerator:
+    @staticmethod
+    def export_tree(
+        document_tree: DocumentTree, traceability_index, output_excel_root
+    ):
+        Path(output_excel_root).mkdir(parents=True, exist_ok=True)
+
+        document: Document
+        for document in document_tree.document_list:
+            document_out_file = "{}/{}.xlsx".format(
+                output_excel_root, document.meta.document_filename_base
+            )
+            ExcelGenerator._export_single_document(
+                document, traceability_index, document_out_file
+            )
+
+    @staticmethod
+    def _export_single_document(
+        document: Document,
+        traceability_index,
+        document_out_file
+    ):
+        with xlsxwriter.Workbook(document_out_file) as workbook:
+            refs = ExcelGenerator._lookup_refs(
+                traceability_index.get_document_iterator(
+                    document
+                ).all_content()
+            )
+            worksheet = workbook.add_worksheet(name=EXCEL_SHEET_NAME)
+
+            parent_header_len = ExcelGenerator._init_columns_width()
+
+            row = 1
+            for node in traceability_index.get_document_iterator(
+                document
+            ).all_content():
+                if not node.is_requirement or not node.uid:
+                    # only export the requirements with uid, allowing tracking
+                    continue
+
+                for idx, column in enumerate(EXPORT_COLUMNS, start=0):
+                    if hasattr(node, column["name"]):
+                        if column["name"] == "statement":
+                            value = node.get_statement_single_or_multiline()
+                        elif column["name"] == "rationale":
+                            value = node.get_rationale_single_or_multiline()
+                        else:
+                            value = getattr(node, column["name"])
+                        worksheet.write(row, idx, value)
+                        if value and len(value) > column[MAX_WIDTH_KEY]:
+                            column[MAX_WIDTH_KEY] = len(value)
+
+                if node.references:
+                    ref = node.references[0]
+                    if len(ref.path) > parent_header_len:
+                        parent_header_len = len(ref.path)
+                    worksheet.write_url(
+                        row,
+                        len(EXPORT_COLUMNS),
+                        f"internal:'{EXCEL_SHEET_NAME}'!A{refs[ref.path]}",
+                        string=ref.path,
+                    )
+                row += 1
+
+            if row == 1:
+                # no requirement with UID
+                print(
+                    "No requirement with UID, nothing to export into excel"
+                )
+            else:
+                # add a table around all this data, allowing filtering and
+                # ordering in Excel
+                worksheet.add_table(
+                    0,
+                    0,
+                    row - 1,
+                    len(EXPORT_COLUMNS),
+                    {"columns": ExcelGenerator._init_headers()},
+                )
+
+                # enforce columns width
+                ExcelGenerator._set_columns_width(
+                    workbook, worksheet, parent_header_len
+                )
+
+        if row == 1:
+            os.unlink(document_out_file)
+
+    @staticmethod
+    def _lookup_refs(document_contents: List) -> Dict[str, int]:
+        refs: Dict[str, int] = {}
+        row = 1
+
+        for content_node in document_contents:
+            if isinstance(content_node, Requirement):
+                if content_node.uid:
+                    # only export the requirements with uid, allowing tracking
+                    refs[content_node.uid] = row + 1
+                    row += 1
+
+        return refs
+
+    @staticmethod
+    def _init_columns_width() -> int:
+        for column in EXPORT_COLUMNS:
+            column[MAX_WIDTH_KEY] = (
+                len(column[COLUMN_HEADER_KEY]) + HEADER_MARGIN
+            )
+        parent_header_len = len(PARENT_COLUMN_HEADER_LABEL) + HEADER_MARGIN
+
+        return parent_header_len
+
+    @staticmethod
+    def _set_columns_width(
+        workbook: Workbook, worksheet: Worksheet, parent_header_len: int
+    ):
+        cell_format_text_wrap = workbook.add_format()
+        cell_format_text_wrap.set_text_wrap()
+
+        for idx, column in enumerate(EXPORT_COLUMNS, start=0):
+            if column[MAX_WIDTH_KEY] > MAX_WIDTH:
+                worksheet.set_column(idx, idx, MAX_WIDTH, cell_format_text_wrap)
+            else:
+                worksheet.set_column(idx, idx, column[MAX_WIDTH_KEY])
+        worksheet.set_column(
+            len(EXPORT_COLUMNS), len(EXPORT_COLUMNS), parent_header_len
+        )
+
+    @staticmethod
+    def _init_headers() -> List:
+        headers: List = []
+
+        for column in EXPORT_COLUMNS:
+            headers.append({"header": column[COLUMN_HEADER_KEY]})
+        headers.append({"header": PARENT_COLUMN_HEADER_LABEL})
+
+        return headers
---
+++ tests/integration/excel_diff.py
@@ -0,0 +1,56 @@
+import argparse
+import itertools
+import os
+import sys
+
+from openpyxl import Workbook, load_workbook
+
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('lhs_file',
+                        type=str,
+                        help='')
+
+arg_parser.add_argument('rhs_file',
+                        type=str,
+                        help='')
+
+args = arg_parser.parse_args()
+
+if not os.path.exists(args.lhs_file):
+    print('error: path does not exist: {}'.format(args.lhs_file),
+          file=sys.stderr)
+    exit(1)
+if not os.path.exists(args.rhs_file):
+    print('error: path does not exist: {}'.format(args.rhs_file),
+          file=sys.stderr)
+    exit(1)
+
+lhs_wb = load_workbook(args.lhs_file)
+rhs_wb = load_workbook(args.rhs_file)
+
+lhs_sheet = lhs_wb.active
+rhs_sheet = rhs_wb.active
+
+if lhs_sheet.max_row != rhs_sheet.max_row:
+    print("Excel files have different number of rows")
+    exit(1)
+if lhs_sheet.max_column != rhs_sheet.max_column:
+    print("Excel files have different number of columns")
+    exit(1)
+
+errors = []
+for row_num in range(1, lhs_sheet.max_row):
+    for col_num in range(1, lhs_sheet.max_column):
+        lhs_cell = lhs_sheet.cell(row=row_num, column=col_num)
+        rhs_cell = rhs_sheet.cell(row=row_num, column=col_num)
+        if lhs_cell.value != rhs_cell.value:
+            errors.append(
+                f"Cell {lhs_cell}: '{lhs_cell.value}' != '{rhs_cell.value}'"
+            )
+
+if errors:
+    print("Excel Diff: files are not equal:")
+    for error in errors:
+        print(error)
+    exit(1)

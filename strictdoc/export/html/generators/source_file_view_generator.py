eaf59f8a661a1c2a440780789d47586bb1332d1b
Stanislav Pankevich <s.pankevich@gmail.com>
2021-02-22 19:04:11 +0100

    export/html: source files: improve <pre> rendering and no last newline

--- strictdoc/export/html/generators/source_file_view_generator.py
+++ strictdoc/export/html/generators/source_file_view_generator.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 from jinja2 import Environment, PackageLoader
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
@@ -40,19 +39,28 @@ class SourceFileViewHTMLGenerator:
         # Ugly hack to split content into lines: Cutting off:
         # <div class="highlight"><pre> and </pre></div>
         # TODO: Implement proper splitting.
+        assert pygmented_source_file_content.startswith(
+            '<div class="highlight"><pre>'
+        )
+        assert pygmented_source_file_content.endswith(
+            "</pre></div>\n"
+        ), f"{pygmented_source_file_content}"
         pygmented_source_file_content = pygmented_source_file_content[
             28 : len(pygmented_source_file_content) - 13
         ]
+        pygmented_source_file_lines = pygmented_source_file_content.split("\n")
+        if pygmented_source_file_lines[-1] == "":
+            pygmented_source_file_lines.pop()
+
         pygments_styles = html_formatter.get_style_defs(".highlight")
         output += template.render(
             source_file=source_file,
             source_file_lines=source_file_lines,
             pygments_styles=pygments_styles,
-            source_file_content=pygmented_source_file_content,
+            source_file_content=pygmented_source_file_lines,
             traceability_index=traceability_index,
             link_renderer=link_renderer,
             renderer=markup_renderer,
             document_type=DocumentType.document(),
         )
-
         return output

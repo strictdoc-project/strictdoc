f20e17f09b74c07362d7580980438f1db2c7f05c
Marina Balioura <mettta@gmail.com>
2021-03-01 15:33:34 +0100

    toc: kepp toc state in sessionStorage

--- strictdoc/export/html/_static/toc.js
+++ strictdoc/export/html/_static/toc.js
@@ -2,38 +2,49 @@
 let tocToggle;
 let tocLayout;
 
-function switchTOC(currState) {
+let setTocState = sessionStorage.getItem('tocState');
+sessionStorage.setItem('tocState', (setTocState ? setTocState : 'close'));
 
-  if(currState == 'open') {
+function switchTOC() {
 
+  const currTocState = sessionStorage.getItem('tocState');
+
+  console.log('switchTOC 1: ', sessionStorage.getItem('tocState'));
+
+  if (currTocState == 'open') {
     tocToggle.dataset.state = 'close';
     tocLayout.dataset.state = 'close';
     console.log('layout: ' + tocLayout.dataset.state);
     console.log('toggle: ' + tocToggle.dataset.state);
 
+    sessionStorage.setItem('tocState', 'close');
   } else {
-
     tocToggle.dataset.state = 'open';
     tocLayout.dataset.state = 'open';
     console.log('layout: ' + tocLayout.dataset.state);
     console.log('toggle: ' + tocToggle.dataset.state);
 
+    sessionStorage.setItem('tocState', 'open');
   }
+
+  console.log('switchTOC 2: ', sessionStorage.getItem('tocState'));
 }
 
-window.onload = function() {
+window.onload = function () {
 
-  // tocToggle = document.getElementById('toc_toggle');
-  // tocLayout = document.getElementById('toc_layout');
+  console.log('onload 1: ', sessionStorage.getItem('tocState'));
 
   tocToggle = document.getElementById('layout_toggle');
   tocLayout = document.getElementById('layout');
 
-  tocToggle.dataset.state = 'open';
-  tocLayout.dataset.state = 'open';
+  const tocState = sessionStorage.getItem('tocState');
+  tocToggle.dataset.state = tocState;
+  tocLayout.dataset.state = tocState;
 
   tocToggle.addEventListener('click', () => {
-    switchTOC(tocToggle.dataset.state);
+    switchTOC();
   });
+
+  console.log('onload 2: ', sessionStorage.getItem('tocState'));
 
 };

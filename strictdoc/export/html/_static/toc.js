8be6d2ae06c27999632f3849defb7bd9881e27f9
Marina Balioura <mettta@gmail.com>
2021-03-08 21:35:34 +0100

    PDF: window.addEventListener in toc.js

--- strictdoc/export/html/_static/toc.js
+++ strictdoc/export/html/_static/toc.js
@@ -1,4 +1,3 @@
-
 let tocToggle;
 let tocLayout;
 
@@ -9,30 +8,20 @@ function switchTOC() {
 
   const currTocState = sessionStorage.getItem('tocState');
 
-  console.log('switchTOC 1: ', sessionStorage.getItem('tocState'));
-
   if (currTocState == 'open') {
     tocToggle.dataset.state = 'close';
     tocLayout.dataset.state = 'close';
-    console.log('layout: ' + tocLayout.dataset.state);
-    console.log('toggle: ' + tocToggle.dataset.state);
 
     sessionStorage.setItem('tocState', 'close');
   } else {
     tocToggle.dataset.state = 'open';
     tocLayout.dataset.state = 'open';
-    console.log('layout: ' + tocLayout.dataset.state);
-    console.log('toggle: ' + tocToggle.dataset.state);
 
     sessionStorage.setItem('tocState', 'open');
   }
-
-  console.log('switchTOC 2: ', sessionStorage.getItem('tocState'));
 }
 
-window.onload = function () {
-
-  console.log('onload 1: ', sessionStorage.getItem('tocState'));
+window.addEventListener("load", function (event) {
 
   tocToggle = document.getElementById('layout_toggle');
   tocLayout = document.getElementById('layout');
@@ -44,7 +33,4 @@ window.onload = function () {
   tocToggle.addEventListener('click', () => {
     switchTOC();
   });
-
-  console.log('onload 2: ', sessionStorage.getItem('tocState'));
-
-};
+});

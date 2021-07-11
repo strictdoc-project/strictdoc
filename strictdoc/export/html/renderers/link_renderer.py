9be8e824ca5a60cd85c70e4512218d267b489979
Marina Balioura <mettta@gmail.com>
2021-07-12 01:28:16 +0200

    export/html: source-code-coverage.js: add Reset for range highlighting

--- strictdoc/export/html/renderers/link_renderer.py
+++ strictdoc/export/html/renderers/link_renderer.py
@@ -78,6 +78,14 @@ class LinkRenderer:
         source_file_link = f"{path_prefix}/_source_files/{document.meta.output_document_dir_rel_path}/{source_file_link}.html"
         return source_file_link
 
+    def render_source_file_link_from_source_file(
+        self, requirement: Requirement, source_file_link: str
+    ):
+        document: Document = requirement.ng_document_reference.get_document()
+        path_prefix = document.meta.get_root_path_prefix()
+        source_file_link = f"{path_prefix}/{document.meta.output_document_dir_rel_path}/{source_file_link}.html#"
+        return source_file_link
+
     def render_requirement_in_source_file_link(
         self,
         requirement: Requirement,

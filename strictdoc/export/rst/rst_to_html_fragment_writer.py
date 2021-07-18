f18ac752347dde893c94e1b0625ff7c0ff7f073c
Stanislav Pankevich <s.pankevich@gmail.com>
2021-07-18 13:15:15 +0200

    export/rst: catch and report RST-to-HTML conversion warnings

--- strictdoc/export/rst/rst_to_html_fragment_writer.py
+++ strictdoc/export/rst/rst_to_html_fragment_writer.py
@@ -1,3 +1,5 @@
+import io
+
 from docutils.core import publish_parts
 
 
@@ -6,5 +8,24 @@ class RstToHtmlFragmentWriter:
     def write(rst_fragment):
         # How do I convert a docutils document tree into an HTML string?
         # https://stackoverflow.com/a/32168938/598057
-        html = publish_parts(rst_fragment, writer_name="html")["html_body"]
+        # Use a io.StringIO as the warning stream to prevent warnings from
+        # being printed to sys.stderr.
+        # https://www.programcreek.com/python/example/88126/docutils.core.publish_parts
+        warning_stream = io.StringIO()
+        settings = {"warning_stream": warning_stream}
+
+        output = publish_parts(
+            rst_fragment, writer_name="html", settings_overrides=settings
+        )
+
+        if warning_stream.tell() > 0:
+            warnings = warning_stream.getvalue().rstrip("\n")
+            print("error: problems when converting RST to HTML:")
+            print(warnings)
+            print("RST fragment: >>>")
+            print(rst_fragment)
+            print("<<<")
+            exit(1)
+
+        html = output["html_body"]
         return html

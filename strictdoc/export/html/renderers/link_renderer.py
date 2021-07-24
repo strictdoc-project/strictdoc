ebc74c4d678cc2b9bae875f6db9cc1a2efe0e303
Stanislav Pankevich <s.pankevich@gmail.com>
2021-07-24 22:42:39 +0200

    export/html: Sec and Req anchors section number to ensure link uniqueness
    Closes #431

--- strictdoc/export/html/renderers/link_renderer.py
+++ strictdoc/export/html/renderers/link_renderer.py
@@ -15,13 +15,18 @@ class LinkRenderer:
     def render_local_anchor(self, node):
         if node in self.local_anchor_cache:
             return self.local_anchor_cache[node]
+        unique_prefix = node.context.title_number_string
         if isinstance(node, Section):
-            local_anchor = self._string_to_link(node.title)
+            local_anchor = f"{unique_prefix}-{self._string_to_link(node.title)}"
         elif isinstance(node, Requirement):
             if node.uid and len(node.uid) > 0:
-                local_anchor = self._string_to_link(node.uid)
+                local_anchor = (
+                    f"{unique_prefix}-{self._string_to_link(node.uid)}"
+                )
             elif node.title and len(node.title) > 0:
-                local_anchor = self._string_to_link(node.title)
+                local_anchor = (
+                    f"{unique_prefix}-{self._string_to_link(node.title)}"
+                )
             else:
                 # TODO: This is not reliable
                 local_anchor = str(id(node))

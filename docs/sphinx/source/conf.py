759c1128abd62156b91cc81782cb1df22a149aa8
Stanislav Pankevich <s.pankevich@gmail.com>
2021-09-05 21:59:21 +0200

    docs: switch to sphinx_rtd_theme for better support by Read the Docs

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -49,7 +49,7 @@ release = STRICTDOC_VERSION
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ["sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -276,7 +276,8 @@ latex_elements = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "bizstyle"
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/sphinx/source/index.rst
+++ docs/sphinx/source/index.rst
@@ -2,6 +2,6 @@ Welcome to StrictDoc's documentation!
 =====================================
 
 .. toctree::
-   :maxdepth: 3
+   :maxdepth: 1
 
    StrictDoc

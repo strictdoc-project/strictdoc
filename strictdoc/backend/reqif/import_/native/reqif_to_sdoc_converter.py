861f687afdf38d92fbb36556b25c32df196f4aa4
Stanislav Pankevich <s.pankevich@gmail.com>
2022-02-19 23:51:18 +0100

    Code climate: backend/reqif: more consistent class naming

--- strictdoc/backend/reqif/import_/native/reqif_to_sdoc_converter.py
+++ strictdoc/backend/reqif/import_/native/reqif_to_sdoc_converter.py
@@ -1,192 +1,179 @@
-from typing import Optional, Union, Dict
+from typing import Optional, List
 
-from reqif.models.reqif_spec_object import ReqIFSpecObject
-from reqif.models.reqif_spec_object_type import SpecAttributeDefinition
+from reqif.models.reqif_data_type import ReqIFDataTypeDefinitionEnumeration
+from reqif.models.reqif_spec_object_type import (
+    ReqIFSpecObjectType,
+)
+from reqif.models.reqif_types import SpecObjectAttributeType
 from reqif.reqif_bundle import ReqIFBundle
 
+from strictdoc.backend.reqif.import_.native.reqif_to_sdoc_factory import (
+    ReqIFToSDocFactory,
+)
 from strictdoc.backend.reqif.sdoc_reqif_fields import (
-    ReqIFChapterField,
     REQIF_MAP_TO_SDOC_FIELD_MAP,
-    ReqIFRequirementReservedField,
+    ReqIFChapterField,
+    DEFAULT_SDOC_GRAMMAR_FIELDS,
 )
 from strictdoc.backend.sdoc.models.document import Document
-from strictdoc.backend.sdoc.models.document_config import DocumentConfig
-from strictdoc.backend.sdoc.models.document_grammar import DocumentGrammar
-from strictdoc.backend.sdoc.models.reference import Reference
+from strictdoc.backend.sdoc.models.document_grammar import (
+    GrammarElement,
+    DocumentGrammar,
+)
 from strictdoc.backend.sdoc.models.requirement import (
     Requirement,
-    RequirementField,
 )
-from strictdoc.backend.sdoc.models.section import Section, FreeText
-from strictdoc.helpers.string import unescape
+from strictdoc.backend.sdoc.models.section import Section
+from strictdoc.backend.sdoc.models.type_system import (
+    GrammarElementFieldString,
+    GrammarElementFieldSingleChoice,
+    GrammarElementFieldMultipleChoice,
+)
 
 
 class ReqIFToSDocConverter:
     @staticmethod
-    def create_document(title: Optional[str]) -> Document:
-        document_config = DocumentConfig(
-            parent=None,
-            version=None,
-            number=None,
-            markup=None,
-            auto_levels=None,
-        )
-        document_title = title if title else "<No title>"
-        document = Document(None, document_title, document_config, None, [], [])
-        document.grammar = DocumentGrammar.create_default(document)
-        return document
+    def convert_reqif_bundle(reqif_bundle: ReqIFBundle):
+        mapping = ReqIFToSDocFactory()
 
-    @staticmethod
-    def is_spec_object_section(
-        spec_object: ReqIFSpecObject, reqif_bundle: ReqIFBundle
-    ):
-        spec_object_type = reqif_bundle.lookup.get_spec_type_by_ref(
-            spec_object.spec_object_type
-        )
-        attribute_map: Dict[
-            str, SpecAttributeDefinition
-        ] = spec_object_type.attribute_map
-        for attribute in spec_object.attributes:
-            long_name_or_none: Optional[str] = attribute_map[
-                attribute.definition_ref
-            ].long_name
-            if long_name_or_none is None:
-                raise NotImplementedError(attribute_map)
-            field_name: str = long_name_or_none
-            if field_name == ReqIFChapterField.CHAPTER_NAME:
-                return True
-        return False
+        # TODO: Should we rather show an error that there are no specifications?
+        if (
+            reqif_bundle.core_content is None
+            or reqif_bundle.core_content.req_if_content is None
+            or len(reqif_bundle.core_content.req_if_content.specifications) == 0
+        ):
+            return mapping.create_document(title=None)
 
-    @staticmethod
-    def is_spec_object_requirement(_):
-        return True
+        specification = reqif_bundle.core_content.req_if_content.specifications[
+            0
+        ]
+        document = mapping.create_document(title=specification.long_name)
 
-    @staticmethod
-    def create_section_from_spec_object(
-        spec_object: ReqIFSpecObject, level, reqif_bundle: ReqIFBundle
-    ) -> Section:
-        spec_object_type = reqif_bundle.lookup.get_spec_type_by_ref(
-            spec_object.spec_object_type
-        )
-        attribute_map: Dict[
-            str, SpecAttributeDefinition
-        ] = spec_object_type.attribute_map
-        assert attribute_map is not None
-        for attribute in spec_object.attributes:
-            field_name_or_none: Optional[str] = attribute_map[
-                attribute.definition_ref
-            ].long_name
-            if field_name_or_none is None:
-                raise NotImplementedError
-            field_name: str = field_name_or_none
-            if field_name == ReqIFChapterField.CHAPTER_NAME:
-                section_title = attribute.value
-                break
-        else:
-            raise NotImplementedError(attribute_map)
+        document.section_contents = []
+        current_section = document
 
-        free_texts = []
-        if ReqIFChapterField.TEXT in spec_object.attribute_map:
-            free_text = unescape(
-                spec_object.attribute_map[ReqIFChapterField.TEXT].value
-            )
-            free_texts.append(
-                FreeText(
-                    parent=None,
-                    parts=[free_text],
+        requirement_spec_type: Optional[ReqIFSpecObjectType] = None
+        for spec_type in reqif_bundle.core_content.req_if_content.spec_types:
+            if isinstance(spec_type, ReqIFSpecObjectType):
+                assert requirement_spec_type is None, (
+                    "Currently only one SPEC-OBJECT-TYPE is supported per "
+                    "ReqIF document: "
+                    f"{reqif_bundle.core_content.req_if_content.spec_types}"
                 )
+                requirement_spec_type = spec_type
+        if requirement_spec_type:
+            attributes = list(requirement_spec_type.attribute_map.keys())
+            if attributes != DEFAULT_SDOC_GRAMMAR_FIELDS:
+                fields = []
+                for attribute in requirement_spec_type.attribute_definitions:
+                    field_name = REQIF_MAP_TO_SDOC_FIELD_MAP.get(
+                        attribute.long_name, attribute.long_name
+                    )
+
+                    # Chapter name is a reserved field for sections.
+                    if field_name == ReqIFChapterField.CHAPTER_NAME:
+                        continue
+                    if (
+                        attribute.attribute_type
+                        == SpecObjectAttributeType.STRING
+                    ):
+                        fields.append(
+                            GrammarElementFieldString(
+                                parent=None,
+                                title=field_name,
+                                required="False",
+                            )
+                        )
+                    elif (
+                        attribute.attribute_type
+                        == SpecObjectAttributeType.XHTML
+                    ):
+                        fields.append(
+                            GrammarElementFieldString(
+                                parent=None,
+                                title=field_name,
+                                required="False",
+                            )
+                        )
+                    elif (
+                        attribute.attribute_type
+                        == SpecObjectAttributeType.ENUMERATION
+                    ):
+                        enum_data_type: ReqIFDataTypeDefinitionEnumeration = (
+                            reqif_bundle.lookup.get_data_type_by_ref(
+                                attribute.datatype_definition
+                            )
+                        )
+                        options = list(
+                            map(lambda v: v.key, enum_data_type.values)
+                        )
+                        if attribute.multi_valued:
+                            fields.append(
+                                GrammarElementFieldMultipleChoice(
+                                    parent=None,
+                                    title=field_name,
+                                    options=options,
+                                    required="False",
+                                )
+                            )
+                        else:
+                            fields.append(
+                                GrammarElementFieldSingleChoice(
+                                    parent=None,
+                                    title=field_name,
+                                    options=options,
+                                    required="False",
+                                )
+                            )
+                    else:
+                        raise NotImplementedError(attribute) from None
+                requirement_element = GrammarElement(
+                    parent=None, tag="REQUIREMENT", fields=fields
+                )
+                elements: List[GrammarElement] = [requirement_element]
+                grammar = DocumentGrammar(parent=document, elements=elements)
+                grammar.is_default = False
+                document.grammar = grammar
+
+        for current_hierarchy in reqif_bundle.iterate_specification_hierarchy(
+            specification
+        ):
+            spec_object = reqif_bundle.get_spec_object_by_ref(
+                current_hierarchy.spec_object
             )
-        section = Section(
-            parent=None,
-            uid=None,
-            level=None,
-            title=section_title,
-            free_texts=free_texts,
-            section_contents=[],
-        )
-        section.ng_level = level
-        return section
 
-    @staticmethod
-    def create_requirement_from_spec_object(
-        spec_object: ReqIFSpecObject,
-        parent_section: Union[Section, Document],
-        reqif_bundle: ReqIFBundle,
-        level,
-    ) -> Requirement:
-        fields = []
-        spec_object_type = reqif_bundle.lookup.get_spec_type_by_ref(
-            spec_object.spec_object_type
-        )
-        attribute_map: Dict[
-            str, SpecAttributeDefinition
-        ] = spec_object_type.attribute_map
-
-        foreign_key_id_or_none: Optional[str] = None
-        for attribute in spec_object.attributes:
-            long_name_or_none = attribute_map[
-                attribute.definition_ref
-            ].long_name
-            if long_name_or_none is None:
-                raise NotImplementedError
-            if long_name_or_none == "ReqIF.ForeignID":
-                foreign_key_id_or_none = attribute.definition_ref
-            field_name: str = long_name_or_none
-            attribute_value = unescape(attribute.value)
-            attribute_multiline_value = None
-            if (
-                "\n" in attribute_value
-                or field_name == ReqIFRequirementReservedField.TEXT
-                or field_name == ReqIFRequirementReservedField.COMMENT_NOTES
+            if mapping.is_spec_object_section(
+                spec_object,
+                reqif_bundle=reqif_bundle,
             ):
-                attribute_multiline_value = attribute_value.lstrip()
-                attribute_value = None
-
-            if field_name in ReqIFRequirementReservedField.SET:
-                field_name = REQIF_MAP_TO_SDOC_FIELD_MAP[field_name]
-            fields.append(
-                RequirementField(
-                    parent=None,
-                    field_name=field_name,
-                    field_value=attribute_value,
-                    field_value_multiline=attribute_multiline_value,
-                    field_value_references=None,
+                section = mapping.create_section_from_spec_object(
+                    spec_object,
+                    current_hierarchy.level,
+                    reqif_bundle=reqif_bundle,
                 )
-            )
-        requirement = Requirement(
-            parent=parent_section, requirement_type="REQUIREMENT", fields=fields
-        )
-        requirement.ng_level = level
-
-        if foreign_key_id_or_none is not None:
-            spec_object_parents = reqif_bundle.get_spec_object_parents(
-                spec_object.identifier
-            )
-            parent_refs = []
-            for spec_object_parent in spec_object_parents:
-                parent_spec_object_parent = (
-                    reqif_bundle.lookup.get_spec_object_by_ref(
-                        spec_object_parent
+                if current_hierarchy.level > current_section.ng_level:
+                    current_section.section_contents.append(section)
+                elif current_hierarchy.level < current_section.ng_level:
+                    for _ in range(
+                        0, current_section.ng_level - current_hierarchy.level
+                    ):
+                        assert not isinstance(current_section, Document)
+                        if isinstance(current_section, Section):
+                            current_section = current_section.parent
+                    current_section.section_contents.append(section)
+                else:
+                    raise NotImplementedError
+            elif mapping.is_spec_object_requirement(spec_object):
+                requirement: Requirement = (
+                    mapping.create_requirement_from_spec_object(
+                        spec_object=spec_object,
+                        parent_section=current_section,
+                        reqif_bundle=reqif_bundle,
+                        level=current_hierarchy.level,
                     )
                 )
+                current_section.section_contents.append(requirement)
+            else:
+                raise NotImplementedError(spec_object) from None
 
-                parent_refs.append(
-                    Reference(
-                        requirement,
-                        "Parent",
-                        parent_spec_object_parent.attribute_map[
-                            foreign_key_id_or_none
-                        ].value,
-                    )
-                )
-            if len(parent_refs) > 0:
-                requirement_field = RequirementField(
-                    parent=requirement,
-                    field_name="REFS",
-                    field_value=None,
-                    field_value_multiline=None,
-                    field_value_references=parent_refs,
-                )
-                fields.append(requirement_field)
-                requirement.ordered_fields_lookup["REFS"] = [requirement_field]
-        return requirement
+        return document
---
+++ strictdoc/backend/reqif/import_/native/reqif_to_sdoc_factory.py
@@ -0,0 +1,192 @@
+from typing import Optional, Union, Dict
+
+from reqif.models.reqif_spec_object import ReqIFSpecObject
+from reqif.models.reqif_spec_object_type import SpecAttributeDefinition
+from reqif.reqif_bundle import ReqIFBundle
+
+from strictdoc.backend.reqif.sdoc_reqif_fields import (
+    ReqIFChapterField,
+    REQIF_MAP_TO_SDOC_FIELD_MAP,
+    ReqIFRequirementReservedField,
+)
+from strictdoc.backend.sdoc.models.document import Document
+from strictdoc.backend.sdoc.models.document_config import DocumentConfig
+from strictdoc.backend.sdoc.models.document_grammar import DocumentGrammar
+from strictdoc.backend.sdoc.models.reference import Reference
+from strictdoc.backend.sdoc.models.requirement import (
+    Requirement,
+    RequirementField,
+)
+from strictdoc.backend.sdoc.models.section import Section, FreeText
+from strictdoc.helpers.string import unescape
+
+
+class ReqIFToSDocFactory:
+    @staticmethod
+    def create_document(title: Optional[str]) -> Document:
+        document_config = DocumentConfig(
+            parent=None,
+            version=None,
+            number=None,
+            markup=None,
+            auto_levels=None,
+        )
+        document_title = title if title else "<No title>"
+        document = Document(None, document_title, document_config, None, [], [])
+        document.grammar = DocumentGrammar.create_default(document)
+        return document
+
+    @staticmethod
+    def is_spec_object_section(
+        spec_object: ReqIFSpecObject, reqif_bundle: ReqIFBundle
+    ):
+        spec_object_type = reqif_bundle.lookup.get_spec_type_by_ref(
+            spec_object.spec_object_type
+        )
+        attribute_map: Dict[
+            str, SpecAttributeDefinition
+        ] = spec_object_type.attribute_map
+        for attribute in spec_object.attributes:
+            long_name_or_none: Optional[str] = attribute_map[
+                attribute.definition_ref
+            ].long_name
+            if long_name_or_none is None:
+                raise NotImplementedError(attribute_map)
+            field_name: str = long_name_or_none
+            if field_name == ReqIFChapterField.CHAPTER_NAME:
+                return True
+        return False
+
+    @staticmethod
+    def is_spec_object_requirement(_):
+        return True
+
+    @staticmethod
+    def create_section_from_spec_object(
+        spec_object: ReqIFSpecObject, level, reqif_bundle: ReqIFBundle
+    ) -> Section:
+        spec_object_type = reqif_bundle.lookup.get_spec_type_by_ref(
+            spec_object.spec_object_type
+        )
+        attribute_map: Dict[
+            str, SpecAttributeDefinition
+        ] = spec_object_type.attribute_map
+        assert attribute_map is not None
+        for attribute in spec_object.attributes:
+            field_name_or_none: Optional[str] = attribute_map[
+                attribute.definition_ref
+            ].long_name
+            if field_name_or_none is None:
+                raise NotImplementedError
+            field_name: str = field_name_or_none
+            if field_name == ReqIFChapterField.CHAPTER_NAME:
+                section_title = attribute.value
+                break
+        else:
+            raise NotImplementedError(attribute_map)
+
+        free_texts = []
+        if ReqIFChapterField.TEXT in spec_object.attribute_map:
+            free_text = unescape(
+                spec_object.attribute_map[ReqIFChapterField.TEXT].value
+            )
+            free_texts.append(
+                FreeText(
+                    parent=None,
+                    parts=[free_text],
+                )
+            )
+        section = Section(
+            parent=None,
+            uid=None,
+            level=None,
+            title=section_title,
+            free_texts=free_texts,
+            section_contents=[],
+        )
+        section.ng_level = level
+        return section
+
+    @staticmethod
+    def create_requirement_from_spec_object(
+        spec_object: ReqIFSpecObject,
+        parent_section: Union[Section, Document],
+        reqif_bundle: ReqIFBundle,
+        level,
+    ) -> Requirement:
+        fields = []
+        spec_object_type = reqif_bundle.lookup.get_spec_type_by_ref(
+            spec_object.spec_object_type
+        )
+        attribute_map: Dict[
+            str, SpecAttributeDefinition
+        ] = spec_object_type.attribute_map
+
+        foreign_key_id_or_none: Optional[str] = None
+        for attribute in spec_object.attributes:
+            long_name_or_none = attribute_map[
+                attribute.definition_ref
+            ].long_name
+            if long_name_or_none is None:
+                raise NotImplementedError
+            if long_name_or_none == "ReqIF.ForeignID":
+                foreign_key_id_or_none = attribute.definition_ref
+            field_name: str = long_name_or_none
+            attribute_value = unescape(attribute.value)
+            attribute_multiline_value = None
+            if (
+                "\n" in attribute_value
+                or field_name == ReqIFRequirementReservedField.TEXT
+                or field_name == ReqIFRequirementReservedField.COMMENT_NOTES
+            ):
+                attribute_multiline_value = attribute_value.lstrip()
+                attribute_value = None
+
+            if field_name in ReqIFRequirementReservedField.SET:
+                field_name = REQIF_MAP_TO_SDOC_FIELD_MAP[field_name]
+            fields.append(
+                RequirementField(
+                    parent=None,
+                    field_name=field_name,
+                    field_value=attribute_value,
+                    field_value_multiline=attribute_multiline_value,
+                    field_value_references=None,
+                )
+            )
+        requirement = Requirement(
+            parent=parent_section, requirement_type="REQUIREMENT", fields=fields
+        )
+        requirement.ng_level = level
+
+        if foreign_key_id_or_none is not None:
+            spec_object_parents = reqif_bundle.get_spec_object_parents(
+                spec_object.identifier
+            )
+            parent_refs = []
+            for spec_object_parent in spec_object_parents:
+                parent_spec_object_parent = (
+                    reqif_bundle.lookup.get_spec_object_by_ref(
+                        spec_object_parent
+                    )
+                )
+
+                parent_refs.append(
+                    Reference(
+                        requirement,
+                        "Parent",
+                        parent_spec_object_parent.attribute_map[
+                            foreign_key_id_or_none
+                        ].value,
+                    )
+                )
+            if len(parent_refs) > 0:
+                requirement_field = RequirementField(
+                    parent=requirement,
+                    field_name="REFS",
+                    field_value=None,
+                    field_value_multiline=None,
+                    field_value_references=parent_refs,
+                )
+                fields.append(requirement_field)
+                requirement.ordered_fields_lookup["REFS"] = [requirement_field]
+        return requirement

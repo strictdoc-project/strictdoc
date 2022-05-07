840cf38ac80c86ef843698e1c8b1b1233465217e
Stanislav Pankevich <s.pankevich@gmail.com>
2022-05-08 01:45:14 +0200

    tests/integration: improve rm.py

--- tests/integration/rm.py
+++ tests/integration/rm.py
@@ -1,6 +1,14 @@
 import os
-import shutil
 import sys
+
+
+def rm_dir(directory):
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+
 
 if len(sys.argv) == 1 or len(sys.argv) != 2:
     print("error: expect one argument: file or folder to be deleted.")
@@ -8,7 +16,7 @@ if len(sys.argv) == 1 or len(sys.argv) != 2:
 
 input_dir_or_file = sys.argv[1]
 if os.path.isdir(input_dir_or_file):
-    shutil.rmtree(input_dir_or_file)
+    rm_dir(input_dir_or_file)
     sys.exit(0)
 
 if os.path.isfile(input_dir_or_file):

155a6019a2eb389fff9a14d8ba2d95a4edb9ca23
Stanislav Pankevich <s.pankevich@gmail.com>
2022-11-20 18:33:22 +0100

    CI: Fix Python 3.10 and 3.11 issues

--- strictdoc/helpers/file_system.py
+++ strictdoc/helpers/file_system.py
@@ -1,15 +1,114 @@
-import distutils.log
-import distutils.dir_util
 import os
+import shutil
 
 
 def sync_dir(src_dir, dst_dir):
     assert os.path.isabs(src_dir), f"Expected {src_dir} to be an absolute path"
     assert os.path.isdir(src_dir), f"Expected {src_dir} to be a directory"
-    distutils.log.set_verbosity(distutils.log.DEBUG)
-    distutils.dir_util.copy_tree(
-        src_dir,
-        dst_dir,
-        update=1,
-        verbose=1,
-    )
+    copytree(src_dir, dst_dir)
+
+
+def copytree(
+    src,
+    dst,
+    symlinks=False,
+    ignore=None,
+    copy_function=shutil.copy2,
+    ignore_dangling_symlinks=False,
+):  # pylint: disable=too-many-arguments
+    """Recursively copy a directory tree.
+
+    The destination directory must not already exist.
+    If exception(s) occur, an Error is raised with a list of reasons.
+
+    If the optional symlinks flag is true, symbolic links in the
+    source tree result in symbolic links in the destination tree; if
+    it is false, the contents of the files pointed to by symbolic
+    links are copied. If the file pointed by the symlink doesn't
+    exist, an exception will be added in the list of errors raised in
+    an Error exception at the end of the copy process.
+
+    You can set the optional ignore_dangling_symlinks flag to true if you
+    want to silence this exception. Notice that this has no effect on
+    platforms that don't support os.symlink.
+
+    The optional ignore argument is a callable. If given, it
+    is called with the `src` parameter, which is the directory
+    being visited by copytree(), and `names` which is the list of
+    `src` contents, as returned by os.listdir():
+
+        callable(src, names) -> ignored_names
+
+    Since copytree() is called recursively, the callable will be
+    called once for each directory that is copied. It returns a
+    list of names relative to the `src` directory that should
+    not be copied.
+
+    The optional copy_function argument is a callable that will be used
+    to copy each file. It will be called with the source path and the
+    destination path as arguments. By default, copy2() is used, but any
+    function that supports the same signature (like copy()) can be used.
+
+    """
+    names = os.listdir(src)
+    if ignore is not None:
+        ignored_names = ignore(src, names)
+    else:
+        ignored_names = set()
+
+    if not os.path.isdir(dst):  # This one line does the trick
+        os.makedirs(dst)
+    errors = []
+    for name in names:
+        if name in ignored_names:
+            continue
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                if symlinks:
+                    # We can't just leave it to `copy_function` because legacy
+                    # code with a custom `copy_function` may rely on copytree
+                    # doing the right thing.
+                    os.symlink(linkto, dstname)
+                    shutil.copystat(
+                        srcname, dstname, follow_symlinks=not symlinks
+                    )
+                else:
+                    # ignore dangling symlink if the flag is on
+                    if not os.path.exists(linkto) and ignore_dangling_symlinks:
+                        continue
+                    # otherwise let the copy occurs. copy2 will raise an error
+                    if os.path.isdir(srcname):
+                        copytree(
+                            srcname, dstname, symlinks, ignore, copy_function
+                        )
+                    else:
+                        copy_function(srcname, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks, ignore, copy_function)
+            else:
+                # Will raise a SpecialFileError for unsupported file types
+                # ---
+                # StrictDoc: We don't want to copy files if they are not newer.
+                if not os.path.isfile(dst) or os.path.getmtime(
+                    src
+                ) > os.path.getmtime(dst):
+                    print(f"Copying: {src} -> {dst}")
+                    copy_function(srcname, dstname)
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except shutil.Error as err:
+            errors.extend(err.args[0])
+        except OSError as why:
+            errors.append((srcname, dstname, str(why)))
+    try:
+        shutil.copystat(src, dst)
+    except OSError as why:
+        # Copying file access times may fail on Windows
+        if getattr(why, "winerror", None) is None:
+            errors.append((src, dst, str(why)))
+    if errors:
+        raise shutil.Error(errors)
+    return dst

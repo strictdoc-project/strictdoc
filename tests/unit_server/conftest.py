83281df096a620fb4892cdaf34595bf09719b130
ggerrits <Guus.Gerrits@mtg.de>
2022-12-12 17:12:17 +0100

    Code climate: Do not rely on SD working dirname in ConfTest

--- tests/unit_server/conftest.py
+++ tests/unit_server/conftest.py
@@ -1,9 +1,7 @@
 import os
 import sys
 
-STRICTDOC_ROOT_PATH = os.path.abspath(
-    os.path.join(__file__, "../../../../strictdoc")
-)
+STRICTDOC_ROOT_PATH = os.path.abspath(os.path.join(__file__, "../../.."))
 assert os.path.exists(STRICTDOC_ROOT_PATH), "does not exist: {}".format(
     STRICTDOC_ROOT_PATH
 )

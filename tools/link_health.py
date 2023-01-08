9a15ff3c6f672a664e52ebf4280ba2af28e2591d
Stanislav Pankevich <s.pankevich@gmail.com>
2023-01-08 14:05:00 +0100

    docs: link health: add Bugseng article to exceptions

--- tools/link_health.py
+++ tools/link_health.py
@@ -1,15 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Link Health checker
+
+Known issues:
+
+1) Some websites respond with HTTP 403 but work fine if a link is opened in a
+browser manually. There must be advanced request validity checkers doing the job
+of rejecting requests sent by bots
+(more advanced request header checks and JS tricks).
+
+The only solution would be to upgrade the Link Health script to use
+Selenium which does a much better job of emulating a real browser. However, for
+simple checking of README pages having few 403 links is fine for our purposes.
+This is why the Link Health still uses basic `requests` API.
+
+Examples:
+
+- https://www.fastcompany.com/28121/they-write-right-stuff
+
+2) Some websites open in a browser but don't give any answer when asked by
+this script. The result is:
+'Connection aborted.', RemoteDisconnected('Remote end closed connection without
+response').
+"""
+
 import argparse
 import multiprocessing
 import os
 import re
 import sys
+import yaml
 from enum import Enum
 from queue import Empty
 from typing import Union, List
 
 import requests
 
-__version__ = "0.0.3"
+__version__ = "0.0.6"
 
 
 class Parallelizer:
@@ -93,21 +120,28 @@ class NullParallelizer:
 
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 }
 
 CONNECT_TIMEOUT = 5
 READ_TIMEOUT = 5
 
 
-class Status(Enum):
-    SUCCESS = 0
-    SUCCESS_READ_TIMEOUT_EXPECTED = 1
-    HTTP_4xx = 2
-    HTTP_5xx = 3
-    CONNECTION_ERROR = 4
-    CONNECT_TIMEOUT = 5
-    READ_TIMEOUT = 6
+class Status(str, Enum):
+    SUCCESS = "SUCCESS"
+    SUCCESS_READ_TIMEOUT_EXPECTED = "SUCCESS_READ_TIMEOUT_EXPECTED"
+    HTTP_4xx = "HTTP_4xx"
+    HTTP_403 = "HTTP_403"
+    HTTP_5xx = "HTTP_5xx"
+    CONNECTION_ERROR = "CONNECTION_ERROR"
+    SSL_ERROR = "SSL_ERROR"
+    CONNECT_TIMEOUT = "CONNECT_TIMEOUT"
+    READ_TIMEOUT = "READ_TIMEOUT"
+
+    @staticmethod
+    def all():
+        return list(map(lambda c: c.value, Status))
 
 
 class ResponseData:
@@ -117,17 +151,26 @@ class ResponseData:
         self.link: str = link
         self.status: Status = status
         self.payload: Union[None, int, str] = payload
+        self.expected = False
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return self.__str__()
 
     @staticmethod
     def create_from_response(
         link: str, response: requests.Response
     ) -> "ResponseData":
         if 200 <= response.status_code <= 400:
-            return ResponseData(link, Status.SUCCESS, response.status_code)
+            return ResponseData(link, Status.SUCCESS, None)
         if 400 <= response.status_code < 500:
-            return ResponseData(link, Status.HTTP_4xx, response.status_code)
+            if response.status_code == 403:
+                return ResponseData(link, Status.HTTP_403, None)
+            return ResponseData(link, Status.HTTP_4xx, None)
         if 500 <= response.status_code < 600:
-            return ResponseData(link, Status.HTTP_5xx, response.status_code)
+            return ResponseData(link, Status.HTTP_5xx, None)
         else:
             raise NotImplementedError(response)
 
@@ -135,6 +178,8 @@ class ResponseData:
     def create_from_exception(
         link: str, exception: Exception
     ) -> "ResponseData":
+        if isinstance(exception, requests.exceptions.SSLError):
+            return ResponseData(link, Status.SSL_ERROR, str(exception))
         if isinstance(exception, requests.exceptions.ReadTimeout):
             return ResponseData(link, Status.READ_TIMEOUT, str(exception))
         if isinstance(exception, requests.exceptions.ConnectTimeout):
@@ -158,6 +203,9 @@ class ResponseData:
             or self.status == Status.SUCCESS_READ_TIMEOUT_EXPECTED
         )
 
+    def is_ssl_error(self):
+        return self.status == Status.SSL_ERROR
+
     def get_error_message(self):
         payload_string = str(self.payload) if self.payload is not None else ""
         return " ".join([str(self.status), payload_string])
@@ -165,6 +213,9 @@ class ResponseData:
     def get_error_message_with_link(self):
         payload_string = str(self.payload) if self.payload is not None else ""
         return " ".join([str(self.status), payload_string, self.link])
+
+    def promote_to_expected(self):
+        self.expected = True
 
 
 def head_request(link) -> ResponseData:
@@ -177,10 +228,13 @@ def head_request(link) -> ResponseData:
         return ResponseData.create_from_exception(link, exception)
 
 
-def get_request(link) -> ResponseData:
+def get_request(*, link, verify: bool) -> ResponseData:
     try:
         response = requests.get(
-            link, timeout=(CONNECT_TIMEOUT, READ_TIMEOUT), headers=HEADERS
+            link,
+            timeout=(CONNECT_TIMEOUT, READ_TIMEOUT),
+            headers=HEADERS,
+            verify=verify,
         )
         return ResponseData.create_from_response(link, response)
     except requests.exceptions.ReadTimeout as exception:
@@ -189,17 +243,40 @@ def get_request(link) -> ResponseData:
         return ResponseData.create_from_exception(link, exception)
 
 
-def check_link(link) -> ResponseData:
+def check_link(link_and_exceptions) -> ResponseData:
+    link = link_and_exceptions[0]
+    exceptions = link_and_exceptions[1]
+
     head_response = head_request(link)
     print(".", end="")
     if head_response.is_success():
         return head_response
 
+    if link in exceptions:
+        code = exceptions[link]
+        if head_response.status.value == code:
+            print(
+                f"\nHEAD {link} -> Known exception: "
+                f"[{head_response.get_error_message()}]"
+            )
+            head_response.promote_to_expected()
+            return head_response
+
     print(f"\nHEAD {link} [{head_response.get_error_message()}]")
 
-    get_response = get_request(link)
+    get_response = get_request(link=link, verify=True)
     if get_response.is_success():
         return get_response
+
+    if get_response.is_ssl_error():
+        print(f"\nGET {link} [{get_response.get_error_message()}]", end="\n")
+        print(
+            f"\nGET {link} – Trying again without SSL verification...", end="\n"
+        )
+
+        get_response = get_request(link=link, verify=False)
+        if get_response.is_success():
+            return get_response
 
     print(f"\nGET {link} [{get_response.get_error_message()}]", end="\n")
 
@@ -207,7 +284,7 @@ def check_link(link) -> ResponseData:
 
 
 def find_links(input_content):
-    return re.findall(rf"(?P<url>https?://[^\s><]+[^\.\s><])", input_content)
+    return re.findall(r"(?P<url>https?://[^\s><]+[^.\s><])", input_content)
 
 
 def main():
@@ -222,19 +299,50 @@ def main():
     with open(input_path, "r") as input_file:
         input_content = input_file.read()
 
+    exceptions = {}
+
+    config_file_name = ".link_health.yml"
+    if os.path.isfile(config_file_name):
+        with open(config_file_name, "r") as stream:
+            try:
+                config_dict = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+                sys.exit(1)
+        if "exceptions" in config_dict:
+            exceptions_dict = config_dict["exceptions"]
+            all_codes = Status.all()
+            for exception_dict in exceptions_dict:
+                assert "url" in exception_dict
+                assert "code" in exception_dict
+                exception_code = exception_dict["code"]
+                if exception_code not in all_codes:
+                    raise Exception(
+                        f"Unknown expected error code: {exception_code}. "
+                        f"Valid codes: {all_codes}"
+                    )
+                exceptions[exception_dict["url"]] = exception_code
+
     links = find_links(input_content)
 
     parallelizer = Parallelizer()
 
-    responses = list(parallelizer.map(links, check_link))
-    print("")
+    link_list = map(lambda el: (el, exceptions), links)
+    responses = list(parallelizer.map(link_list, check_link))
 
-    def is_failed_response(response: ResponseData):
-        return not response.is_success()
-
-    failed_responses: List[ResponseData] = list(
-        filter(is_failed_response, responses)
+    expected_failed_responses: List[ResponseData] = list(
+        filter(lambda r: not r.is_success() and r.expected, responses)
     )
+    failed_responses: List[ResponseData] = list(
+        filter(lambda r: not r.is_success() and not r.expected, responses)
+    )
+
+    print("\n")
+    for expected_failed_response in expected_failed_responses:
+        print(
+            f"Expectedly failed link: "
+            f"{expected_failed_response.get_error_message_with_link()}"
+        )
     for failed_response in failed_responses:
         print(f"Failed link: {failed_response.get_error_message_with_link()}")
     print(

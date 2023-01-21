b04d06068609948aecd9e4812a02b797bfca8145
Stanislav Pankevich <s.pankevich@gmail.com>
2023-01-21 23:38:33 +0100

    tests/end2end: edit document config: switch to calling links via @title

--- tests/end2end/document/UC11_edit_document_config/20_editing_document_uid/test_20_editing_document_uid.py
+++ tests/end2end/document/UC11_edit_document_config/20_editing_document_uid/test_20_editing_document_uid.py
@@ -33,7 +33,7 @@ class Test_UC11_20_EditDocumentUID(BaseCase):
         self.assert_text_visible("Requirement title")
 
         self.click_nth_visible_element(
-            "//a[contains(text(), 'Edit document config')]", 1
+            "//a[@title='Edit document configuration']", 1
         )
 
         self.type("(//div[@id='document[UID]'])[1]", "DOC_001", by=By.XPATH)

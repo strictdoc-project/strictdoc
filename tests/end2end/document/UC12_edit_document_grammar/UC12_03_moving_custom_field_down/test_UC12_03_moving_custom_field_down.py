630e7dade3a83373f5f043054d925064bd815696
Stanislav Pankevich <s.pankevich@gmail.com>
2023-01-22 00:15:53 +0100

    server: fix editing of the grammar fields

---
+++ tests/end2end/document/UC12_edit_document_grammar/UC12_03_moving_custom_field_down/test_UC12_03_moving_custom_field_down.py
@@ -0,0 +1,47 @@
+import filecmp
+import os
+import shutil
+
+from seleniumbase import BaseCase
+
+from tests.end2end.server import SDocTestServer
+
+path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
+
+
+class Test_UC12_03_MovingCustomFieldDown(BaseCase):
+    def test_01(self):
+        path_to_sandbox = os.path.join(
+            path_to_this_test_file_folder, ".sandbox"
+        )
+
+        test_server = SDocTestServer.create(path_to_sandbox)
+        shutil.copyfile(
+            os.path.join(path_to_this_test_file_folder, "document.sdoc"),
+            os.path.join(path_to_sandbox, "document.sdoc"),
+        )
+
+        test_server.run()
+
+        self.open("http://localhost:8001")
+
+        self.assert_text("Document 1")
+        self.assert_text("PROJECT INDEX")
+
+        self.click_link("DOC")
+        self.assert_text_visible("Requirement title")
+
+        self.click_link("Edit document grammar")
+
+        self.click_xpath("(//a[@title='Move down'])[last()]")
+
+        self.click_xpath("//button[@type='submit' and text()='Save']")
+        self.assert_text_not_visible("Save")
+
+        assert os.path.exists(os.path.join(path_to_sandbox, "document.sdoc"))
+        assert filecmp.cmp(
+            os.path.join(path_to_sandbox, "document.sdoc"),
+            os.path.join(
+                path_to_this_test_file_folder, "document.expected.sdoc"
+            ),
+        )

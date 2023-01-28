e5407e0b2bd85a194845ccf56c6955bb733ce144
Stanislav Pankevich <s.pankevich@gmail.com>
2023-01-28 19:39:36 +0100

    Code climate: tests/end2end: remove debug screenshot lines

--- tests/end2end/document_tree/20_export_tree_to_reqif/test_20_export_tree_to_reqif.py
+++ tests/end2end/document_tree/20_export_tree_to_reqif/test_20_export_tree_to_reqif.py
@@ -35,7 +35,6 @@ class Test_20_ExportTreeToReqIF(BaseCase):
         test_server.run()
 
         self.open(test_server.get_host_and_port())
-        self.save_screenshot_to_logs()
 
         self.assert_text("Document 1")
         self.assert_text("PROJECT INDEX")

317366f3acf547c7deba89a8c8ee4966073fda25
Stanislav Pankevich <s.pankevich@gmail.com>
2023-01-29 10:48:33 +0100

    Code climate: tests/end2end: edit requirement: update folder/file/test names to latest conventions

--- tests/end2end/document/UC07_editing_requirement/UC07_T02_cancel_edit_requirement/test_UC07_T02_cancel_edit_requirement.py
+++ tests/end2end/document/UC07_editing_requirement/UC07_T02_cancel_edit_requirement/test_UC07_T02_cancel_edit_requirement.py
@@ -1,4 +1,3 @@
-import filecmp
 import os
 import shutil
 
@@ -10,7 +9,7 @@ from tests.end2end.server import SDocTestServer
 path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
 
 
-class Test07EditRequirement(BaseCase):
+class Test_UC07_T02_CancelEditRequirement(BaseCase):
     def test_01(self):
         path_to_sandbox = os.path.join(
             path_to_this_test_file_folder, ".sandbox"
@@ -42,26 +41,5 @@ class Test07EditRequirement(BaseCase):
             click_by=By.XPATH,
         )
 
-        self.type("#requirement_UID", "Modified_UID")
-        self.type("#requirement_TITLE", "Modified title")
-        self.type("#requirement_STATEMENT", "Modified statement.")
-        self.type("#requirement_RATIONALE", "Modified rationale.")
-
-        self.click_xpath("//button[@type='submit' and text()='Save']")
-
-        self.assert_text("1. Modified title")
-        self.assert_text("Modified_UID")
-        self.assert_text("Modified statement.")
-        self.assert_text("Modified rationale.")
-
-        self.assert_element(
-            "//turbo-frame[@id='frame-toc']//*[contains(., 'Modified title')]"
-        )
-
-        assert os.path.exists(os.path.join(path_to_sandbox, "document.sdoc"))
-        assert filecmp.cmp(
-            os.path.join(path_to_sandbox, "document.sdoc"),
-            os.path.join(
-                path_to_this_test_file_folder, "document.expected.sdoc"
-            ),
-        )
+        self.click_link("Cancel")
+        self.assert_text_not_visible("Cancel")

cd2664634a2767a62612d113d1b9051d086ba8a3
Stanislav Pankevich <s.pankevich@gmail.com>
2023-02-22 20:25:30 +0100

    examples: move to the separate examples repository
    https://github.com/strictdoc-project/strictdoc-examples

--- examples/example_source_files/file.py
+++
@@ -1,8 +0,0 @@
-def hello_world():
-    print("hello world")
-
-
-# [REQ-002]
-def hello_world_2():
-    print("hello world")
-# [/REQ-002]
--- examples/example_source_files/file2.py
+++
@@ -1,9 +0,0 @@
-
-def hello_world():
-    print("hello world")
-
-
-# [REQ-003]
-def hello_world_3():
-    print("hello world")
-# [/REQ-003]

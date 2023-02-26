f6603d6377f096a6e5077c9c46a0625e03be7866
Stanislav Pankevich <s.pankevich@gmail.com>
2023-02-26 16:29:36 +0100

    export/rst: improve linking sections by using their UID

--- strictdoc/export/rst/writer.py
+++ strictdoc/export/rst/writer.py
@@ -1,5 +1,7 @@
 from enum import Enum
+from typing import Optional
 
+from strictdoc.backend.sdoc.models.document import Document
 from strictdoc.backend.sdoc.models.inline_link import InlineLink
 from strictdoc.backend.sdoc.models.requirement import Requirement
 from strictdoc.backend.sdoc.models.section import FreeText, Section
@@ -18,12 +20,17 @@ class RSTWriter:
     def __init__(self, index: TraceabilityIndex):
         self.index = index
 
-    def write(self, document, single_document):
+    def write(self, document: Document, single_document):
         document_iterator = DocumentCachingIterator(document)
         output = ""
 
         if not single_document:
-            output += self._print_rst_header(document.title, 0)
+            document_uid: Optional[str] = None
+            if document.config.uid is not None:
+                document_uid = document.config.uid
+            output += self._print_rst_header(
+                document.title, 0, reference_uid=document_uid
+            )
 
         for free_text in document.free_texts:
             output += self._print_free_text(free_text)
@@ -31,7 +38,9 @@ class RSTWriter:
         for content_node in document_iterator.all_content():
             if isinstance(content_node, Section):
                 output += self._print_rst_header(
-                    content_node.title, content_node.ng_level
+                    content_node.title,
+                    content_node.ng_level,
+                    content_node.reserved_uid,
                 )
                 for free_text in content_node.free_texts:
                     output += self._print_free_text(free_text)
@@ -65,7 +74,9 @@ class RSTWriter:
         return output
 
     @staticmethod
-    def _print_rst_header(string: str, level: int):
+    def _print_rst_header(
+        string: str, level: int, reference_uid: Optional[str]
+    ):
         assert isinstance(string, str), string
         assert isinstance(level, int), level
         chars = {
@@ -80,6 +91,13 @@ class RSTWriter:
         }
         header_char = chars[level]
         output = ""
+
+        # An RST reference looks like this:
+        # .. _SDOC-HIGH-VALIDATION:
+        if reference_uid is not None:
+            assert len(reference_uid) > 0, reference_uid
+            output += f".. _{reference_uid}:\n\n"
+
         output += string
         output += "\n"
         output += header_char.rjust(len(string), header_char)
@@ -106,8 +124,7 @@ class RSTWriter:
             if isinstance(part, str):
                 output += part
             elif isinstance(part, InlineLink):
-                node = self.index.get_node_by_uid(part.link)
-                output += f"`{node.title}`_"
+                output += f":ref:`{part.link}`"
             else:
                 raise NotImplementedError
         output += "\n"

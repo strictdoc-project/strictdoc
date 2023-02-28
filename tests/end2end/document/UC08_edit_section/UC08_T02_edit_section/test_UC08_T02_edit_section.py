db41f44eb2aad24284b67b0aaf2e68e973226848
Stanislav Pankevich <s.pankevich@gmail.com>
2023-02-28 20:37:52 +0100

    tests/end2end: migrate tests to safe server code: UC08

--- tests/end2end/document/UC08_edit_section/UC08_T02_edit_section/test_UC08_T02_edit_section.py
+++ tests/end2end/document/UC08_edit_section/UC08_T02_edit_section/test_UC08_T02_edit_section.py
@@ -1,63 +1,46 @@
-import filecmp
-import os
-import shutil
-
 from selenium.webdriver.common.by import By
 from seleniumbase import BaseCase
 
+from tests.end2end.end2end_test_setup import End2EndTestSetup
 from tests.end2end.server import SDocTestServer
-
-path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
 
 
 class Test_UC08_T02_EditSection(BaseCase):
     def test_01(self):
-        path_to_sandbox = os.path.join(
-            path_to_this_test_file_folder, ".sandbox"
-        )
+        test_setup = End2EndTestSetup(path_to_test_file=__file__)
 
-        test_server = SDocTestServer.create(path_to_sandbox)
-        shutil.copyfile(
-            os.path.join(path_to_this_test_file_folder, "document.sdoc"),
-            os.path.join(path_to_sandbox, "document.sdoc"),
-        )
+        with SDocTestServer(
+            input_path=test_setup.path_to_sandbox
+        ) as test_server:
+            self.open(test_server.get_host_and_port())
 
-        test_server.run()
+            self.assert_text("Document 1")
+            self.assert_text("PROJECT INDEX")
 
-        self.open(test_server.get_host_and_port())
+            self.click_xpath('//*[@data-testid="tree-file-link"]')
 
-        self.assert_text("Document 1")
-        self.assert_text("PROJECT INDEX")
+            self.assert_text("Hello world!")
 
-        self.click_xpath('//*[@data-testid="tree-file-link"]')
+            self.hover_and_click(
+                hover_selector="(//sdoc-node)[2]",
+                click_selector=(
+                    '(//sdoc-node)[2]//*[@data-testid="node-edit-action"]'
+                ),
+                hover_by=By.XPATH,
+                click_by=By.XPATH,
+            )
 
-        self.assert_text("Hello world!")
+            self.type("#section_title", "Modified title")
+            self.type("#section_content", "Modified statement.")
 
-        self.hover_and_click(
-            hover_selector="(//sdoc-node)[2]",
-            click_selector=(
-                '(//sdoc-node)[2]//*[@data-testid="node-edit-action"]'
-            ),
-            hover_by=By.XPATH,
-            click_by=By.XPATH,
-        )
+            self.click_xpath('//*[@data-testid="form-submit-action"]')
 
-        self.type("#section_title", "Modified title")
-        self.type("#section_content", "Modified statement.")
+            self.assert_text("1. Modified title")
+            self.assert_text("Modified statement.")
 
-        self.click_xpath('//*[@data-testid="form-submit-action"]')
+            self.assert_element(
+                "//turbo-frame[@id='frame-toc']"
+                "//*[contains(., 'Modified title')]"
+            )
 
-        self.assert_text("1. Modified title")
-        self.assert_text("Modified statement.")
-
-        self.assert_element(
-            "//turbo-frame[@id='frame-toc']//*[contains(., 'Modified title')]"
-        )
-
-        assert os.path.exists(os.path.join(path_to_sandbox, "document.sdoc"))
-        assert filecmp.cmp(
-            os.path.join(path_to_sandbox, "document.sdoc"),
-            os.path.join(
-                path_to_this_test_file_folder, "document.expected.sdoc"
-            ),
-        )
+        assert test_setup.compare_sandbox_and_expected_output()

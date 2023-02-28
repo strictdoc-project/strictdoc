4b3ca9530d13aae91c515ea958442b74f271be2f
Stanislav Pankevich <s.pankevich@gmail.com>
2023-02-28 20:06:35 +0100

    tests/end2end: migrate tests to safe server code: UC07
    Relates to #925

--- tests/end2end/document/UC07_edit_requirement/UC07_T02_editing_statement_only/test_UC07_T02_editing_statement_only.py
+++ tests/end2end/document/UC07_edit_requirement/UC07_T02_editing_statement_only/test_UC07_T02_editing_statement_only.py
@@ -1,70 +1,56 @@
-import filecmp
-import os
-import shutil
-
 from selenium.webdriver.common.by import By
 from seleniumbase import BaseCase
 
+from tests.end2end.end2end_test_setup import End2EndTestSetup
 from tests.end2end.server import SDocTestServer
-
-path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
 
 
 class Test_UC07_T02_EditingStatementOnly(BaseCase):
     def test_01(self):
-        path_to_sandbox = os.path.join(
-            path_to_this_test_file_folder, ".sandbox"
-        )
+        test_setup = End2EndTestSetup(path_to_test_file=__file__)
 
-        test_server = SDocTestServer.create(path_to_sandbox)
-        shutil.copyfile(
-            os.path.join(path_to_this_test_file_folder, "document.sdoc"),
-            os.path.join(path_to_sandbox, "document.sdoc"),
-        )
+        with SDocTestServer(
+            input_path=test_setup.path_to_sandbox
+        ) as test_server:
+            self.open(test_server.get_host_and_port())
 
-        test_server.run()
+            self.assert_text("Document 1")
+            self.assert_text("PROJECT INDEX")
 
-        self.open(test_server.get_host_and_port())
+            self.click_xpath('//*[@data-testid="tree-file-link"]')
 
-        self.assert_text("Document 1")
-        self.assert_text("PROJECT INDEX")
+            self.assert_text("Hello world!")
+            # Make sure that the normal (not table-based) requirement is
+            # rendered.
+            self.assert_element(
+                '//sdoc-node[@data-testid="node-requirement-normal"]',
+                by=By.XPATH,
+            )
 
-        self.click_xpath('//*[@data-testid="tree-file-link"]')
+            self.hover_and_click(
+                hover_selector="(//sdoc-node)[2]",
+                click_selector=(
+                    '(//sdoc-node)[2]//*[@data-testid="node-edit-action"]'
+                ),
+                hover_by=By.XPATH,
+                click_by=By.XPATH,
+            )
 
-        self.assert_text("Hello world!")
-        # Make sure that the normal (not table-based) requirement is rendered.
-        self.assert_element(
-            '//sdoc-node[@data-testid="node-requirement-normal"]', by=By.XPATH
-        )
+            self.type(
+                "//*[@id='requirement[STATEMENT]']",
+                "Modified statement.",
+                by=By.XPATH,
+            )
 
-        self.hover_and_click(
-            hover_selector="(//sdoc-node)[2]",
-            click_selector=(
-                '(//sdoc-node)[2]//*[@data-testid="node-edit-action"]'
-            ),
-            hover_by=By.XPATH,
-            click_by=By.XPATH,
-        )
+            self.click_xpath('//*[@data-testid="form-submit-action"]')
 
-        self.type(
-            "//*[@id='requirement[STATEMENT]']",
-            "Modified statement.",
-            by=By.XPATH,
-        )
+            self.assert_text("Modified statement.")
 
-        self.click_xpath('//*[@data-testid="form-submit-action"]')
+            # Make sure that the normal (not table-based) requirement is
+            # rendered.
+            self.assert_element(
+                '//sdoc-node[@data-testid="node-requirement-normal"]',
+                by=By.XPATH,
+            )
 
-        self.assert_text("Modified statement.")
-
-        # Make sure that the normal (not table-based) requirement is rendered.
-        self.assert_element(
-            '//sdoc-node[@data-testid="node-requirement-normal"]', by=By.XPATH
-        )
-
-        assert os.path.exists(os.path.join(path_to_sandbox, "document.sdoc"))
-        assert filecmp.cmp(
-            os.path.join(path_to_sandbox, "document.sdoc"),
-            os.path.join(
-                path_to_this_test_file_folder, "document.expected.sdoc"
-            ),
-        )
+        assert test_setup.compare_sandbox_and_expected_output()

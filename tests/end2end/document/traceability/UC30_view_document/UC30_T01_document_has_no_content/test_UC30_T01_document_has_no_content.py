fed29d943e5ff0bc88c3699a044dd8e7b6d37ae7
Maryna Balioura <mettta@gmail.com>
2023-03-13 19:36:04 +0100

    tests/end2end: add table and traceability initial tests

---
+++ tests/end2end/document/traceability/UC30_view_document/UC30_T01_document_has_no_content/test_UC30_T01_document_has_no_content.py
@@ -0,0 +1,30 @@
+from seleniumbase import BaseCase
+
+from tests.end2end.end2end_test_setup import End2EndTestSetup
+from tests.end2end.helpers.screens.document_tree.screen_document_tree import (
+    Screen_DocumentTree,
+)
+from tests.end2end.server import SDocTestServer
+
+
+class Test_UC30_T01_document_has_no_content(BaseCase):
+    def test_01(self):
+        test_setup = End2EndTestSetup(path_to_test_file=__file__)
+
+        with SDocTestServer(
+            input_path=test_setup.path_to_sandbox
+        ) as test_server:
+            self.open(test_server.get_host_and_port())
+
+            screen_document_tree = Screen_DocumentTree(self)
+
+            screen_document_tree.assert_on_screen()
+            screen_document_tree.assert_contains_document("Document title")
+
+            screen_document = screen_document_tree.do_click_on_first_document()
+
+            screen_document.assert_on_screen()
+            screen_document.assert_is_document_title("Document title")
+            screen_document.assert_text("Hello world!")
+
+        assert test_setup.compare_sandbox_and_expected_output()

7f1389e18efef003f6add3b5dbe5c3f72835352c
Maryna Balioura <mettta@gmail.com>
2023-04-22 16:36:57 +0200

    export/html: resizable-bar.js: update style() and remove unused constants

--- strictdoc/export/html/_static/resizable_bar.js
+++ strictdoc/export/html/_static/resizable_bar.js
@@ -9,212 +9,31 @@
 // ! Right now, the code only considers one possibility for the panels
 // ! to the left of the main part.
 
-const BAR_ATTRIBUTE = 'js-resizable_bar';
-const BAR_MIN_WIDTH = 100;
-const BAR_CLOSED_WIDTH = 12;
-const BAR_MAX_VW = '25vw';
-
-const BAR_HANDLER_WIDTH = 8;
-
-const BAR_COLOR_MAIN = 'var(--color-fg-main, Black)';
-const BAR_COLOR_BACKGROUND = 'var(--color-bg-main, White)';
-const BAR_COLOR_ACTIVE = 'var(--color-fg-accent, currentColor)';
-const BAR_COLOR_BORDER = 'var(--color-border, rgba(0,0,0,0.1))';
-
-const STYLE = `
-[${BAR_ATTRIBUTE}]::after {
-  opacity: 0;
-  transition: .3s;
-  pointer-events: none;
-}
-
-[${BAR_ATTRIBUTE}] {
-  position: relative;
-  height: 100%;
-  max-width: ${BAR_MAX_VW};
-}
-
-[${BAR_ATTRIBUTE}]:hover {
-  z-index: 22;
-}
-
-[${BAR_ATTRIBUTE}][data-state="open"] {
-  min-width: ${BAR_MIN_WIDTH}px;
-  pointer-events: auto;
-}
-[${BAR_ATTRIBUTE}][data-state="closed"] {
-  max-width: ${BAR_CLOSED_WIDTH}px;
-  min-width: ${BAR_CLOSED_WIDTH}px;
-  pointer-events: none;
-  transition: .5s;
-}
-
-[${BAR_ATTRIBUTE}][data-position="left"] {
-  border-left: none;
-}
-[${BAR_ATTRIBUTE}][data-position="right"] {
-  border-right: none;
-}
-
-[${BAR_ATTRIBUTE}-handler] {
-  pointer-events: auto;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 10;
-  width: ${BAR_HANDLER_WIDTH}px;
-  color: ${BAR_COLOR_ACTIVE};
-}
-
-[${BAR_ATTRIBUTE}-border] {
-  pointer-events: auto;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: ${BAR_HANDLER_WIDTH}px;
-  background: transparent;
-  transition: .3s;
-  cursor: col-resize;
-}
-
-[${BAR_ATTRIBUTE}][data-state="closed"] [${BAR_ATTRIBUTE}-border] {
-  cursor: e-resize;
-}
-
-[${BAR_ATTRIBUTE}-border]::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 1px;
-  background: ${BAR_COLOR_BORDER};
-  transition: .3s;
-}
-
-[${BAR_ATTRIBUTE}-border]::after {
-  content: '';
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: ${0.5 * BAR_HANDLER_WIDTH}px;
-  background: transparent;
-  transition: .3s;
-}
-
-[${BAR_ATTRIBUTE}][data-position="left"] [${BAR_ATTRIBUTE}-border]::before {
-  right: 0;
-  left: unset;
-}
-[${BAR_ATTRIBUTE}][data-position="left"] [${BAR_ATTRIBUTE}-border]::after {
-  right: ${-0.25 * BAR_HANDLER_WIDTH}px;
-  left: unset;
-}
-
-[${BAR_ATTRIBUTE}][data-position="right"] [${BAR_ATTRIBUTE}-border]::before {
-  right: unset;
-  left: 0;
-}
-[${BAR_ATTRIBUTE}][data-position="right"] [${BAR_ATTRIBUTE}-border]::after {
-  right: unset;
-  left: ${-0.25 * BAR_HANDLER_WIDTH}px;
-}
-
-[${BAR_ATTRIBUTE}-border]:hover::after {
-  background: ${BAR_COLOR_ACTIVE};
-}
-
-[${BAR_ATTRIBUTE}-button] {
-  cursor: pointer;
-  position: absolute;
-  z-index: 2;
-  left: 0;
-  top: ${-1 * BAR_HANDLER_WIDTH}px;
-  box-sizing: border-box;
-  width: ${2 * BAR_HANDLER_WIDTH}px;
-  height: ${2 * BAR_HANDLER_WIDTH}px;
-  font-size: ${1.5 * BAR_HANDLER_WIDTH}px;
-  font-weight: bold;
-  border-radius: 50%;
-  border-width: 1px;
-  border-style: solid;
-  border-color: ${BAR_COLOR_BACKGROUND};
-  background: ${BAR_COLOR_BACKGROUND};
-  color: ${BAR_COLOR_ACTIVE};
-  transition: .3s;
-}
-
-[${BAR_ATTRIBUTE}-button]:hover {
-  color: ${BAR_COLOR_MAIN};
-  border-color: ${BAR_COLOR_MAIN};
-}
-
-/* ❮❯ */
-[${BAR_ATTRIBUTE}-button]::after {
-  content: '❮';
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  left: 0;
-  right: 0;
-  top: ${-1 * BAR_HANDLER_WIDTH}px;
-  bottom: ${-1 * BAR_HANDLER_WIDTH}px;
-}
-[${BAR_ATTRIBUTE}][data-state="open"] [${BAR_ATTRIBUTE}-button]::after {
-  content: '❮';
-}
-[${BAR_ATTRIBUTE}][data-state="closed"] [${BAR_ATTRIBUTE}-button]::after {
-  content: '❯';
-}
-
-[${BAR_ATTRIBUTE}-scroll] {
-  height: 100%;
-  overflow-x: hidden;
-  overflow-y: scroll;
-  padding: calc(var(--base-rhythm)*2);
-  padding-bottom: calc(var(--base-rhythm)*8);
-  scrollbar-color: transparent var(--scrollbarBG);
-  /* scrollbar-width: thin; */
-}
-[${BAR_ATTRIBUTE}-scroll='y'] {
-  overflow-x: hidden;
-  overflow-y: scroll;
-}
-[${BAR_ATTRIBUTE}-scroll]:hover {
-  scrollbar-color: var(--thumbBG) var(--scrollbarBG);
-}
-[${BAR_ATTRIBUTE}-scroll]::-webkit-scrollbar {
-  /* width: var(--base-rhythm); */
-}
-[${BAR_ATTRIBUTE}-scroll]::-webkit-scrollbar-thumb {
-  background-color: transparent;
-}
-[${BAR_ATTRIBUTE}-scroll]:hover::-webkit-scrollbar-thumb {
-  background-color: var(--thumbBG)
-}
-
-[data-state="closed"] [${BAR_ATTRIBUTE}-scroll] {
-  display: none;
-}
-`;
-
 class ResizableBar {
   constructor({
     barAttribute,
-    barStyle,
     barGravity,
+    barMaxWidthVW,
+    // styles
+    barMinWidth,
+    barClosedWidth,
+    barHandlerWidth,
+    barColorMain,
+    barColorBackground,
+    barColorActive,
+    barColorBorder,
   }) {
     this.barAttribute = barAttribute || 'js-resizable_bar';
-    this.barStyle = barStyle;
+    this.barMaxWidthVW = barMaxWidthVW || '25vw';
     this.barGravity = barGravity || 100;
+    // styles
+    this.barMinWidth = barMinWidth || 100;
+    this.barClosedWidth = barClosedWidth || 12;
+    this.barHandlerWidth = barHandlerWidth || 8;
+    this.barColorMain = barColorMain || 'var(--color-fg-main, Black)';
+    this.barColorBackground = barColorBackground || 'var(--color-bg-main, White)';
+    this.barColorActive = barColorActive || 'var(--color-fg-accent, currentColor)';
+    this.barColorBorder = barColorBorder || 'var(--color-border, rgba(0,0,0,0.1))';
 
     // state
     this.state = [];
@@ -340,11 +159,196 @@ class ResizableBar {
 
   _insertStyle() {
     const style = document.createElement('style');
-    // style.id = 'resizable_bar-style';
-    style.setAttribute("resizable_bar-style", '');
-    style.textContent = this.barStyle;
-    // Add style
+    style.setAttribute(`${this.barAttribute}-style`, '');
+    style.textContent = this._style();
     document.head.append(style);
+  }
+
+  _style() {
+    return `
+[${this.barAttribute}]::after {
+  opacity: 0;
+  transition: .3s;
+  pointer-events: none;
+}
+
+[${this.barAttribute}] {
+  position: relative;
+  height: 100%;
+  max-width: ${this.barMaxWidthVW};
+}
+
+[${this.barAttribute}]:hover {
+  z-index: 22;
+}
+
+[${this.barAttribute}][data-state="open"] {
+  min-width: ${this.barMinWidth}px;
+  pointer-events: auto;
+}
+[${this.barAttribute}][data-state="closed"] {
+  max-width: ${this.barClosedWidth}px;
+  min-width: ${this.barClosedWidth}px;
+  pointer-events: none;
+  transition: .5s;
+}
+
+[${this.barAttribute}][data-position="left"] {
+  border-left: none;
+}
+[${this.barAttribute}][data-position="right"] {
+  border-right: none;
+}
+
+[${this.barAttribute}-handler] {
+  pointer-events: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  width: ${this.barHandlerWidth}px;
+  color: ${this.barColorActive};
+}
+
+[${this.barAttribute}-border] {
+  pointer-events: auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: ${this.barHandlerWidth}px;
+  background: transparent;
+  transition: .3s;
+  cursor: col-resize;
+}
+
+[${this.barAttribute}][data-state="closed"] [${this.barAttribute}-border] {
+  cursor: e-resize;
+}
+
+[${this.barAttribute}-border]::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 1px;
+  background: ${this.barColorBorder};
+  transition: .3s;
+}
+
+[${this.barAttribute}-border]::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: ${0.5 * this.barHandlerWidth}px;
+  background: transparent;
+  transition: .3s;
+}
+
+[${this.barAttribute}][data-position="left"] [${this.barAttribute}-border]::before {
+  right: 0;
+  left: unset;
+}
+[${this.barAttribute}][data-position="left"] [${this.barAttribute}-border]::after {
+  right: ${-0.25 * this.barHandlerWidth}px;
+  left: unset;
+}
+
+[${this.barAttribute}][data-position="right"] [${this.barAttribute}-border]::before {
+  right: unset;
+  left: 0;
+}
+[${this.barAttribute}][data-position="right"] [${this.barAttribute}-border]::after {
+  right: unset;
+  left: ${-0.25 * this.barHandlerWidth}px;
+}
+
+[${this.barAttribute}-border]:hover::after {
+  background: ${this.barColorActive};
+}
+
+[${this.barAttribute}-button] {
+  cursor: pointer;
+  position: absolute;
+  z-index: 2;
+  left: 0;
+  top: ${-1 * this.barHandlerWidth}px;
+  box-sizing: border-box;
+  width: ${2 * this.barHandlerWidth}px;
+  height: ${2 * this.barHandlerWidth}px;
+  font-size: ${1.5 * this.barHandlerWidth}px;
+  font-weight: bold;
+  border-radius: 50%;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${this.barColorBackground};
+  background: ${this.barColorBackground};
+  color: ${this.barColorActive};
+  transition: .3s;
+}
+
+[${this.barAttribute}-button]:hover {
+  color: ${this.barColorMain};
+  border-color: ${this.barColorMain};
+}
+
+/* ❮❯ */
+[${this.barAttribute}-button]::after {
+  content: '❮';
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: 0;
+  right: 0;
+  top: ${-1 * this.barHandlerWidth}px;
+  bottom: ${-1 * this.barHandlerWidth}px;
+}
+[${this.barAttribute}][data-state="open"] [${this.barAttribute}-button]::after {
+  content: '❮';
+}
+[${this.barAttribute}][data-state="closed"] [${this.barAttribute}-button]::after {
+  content: '❯';
+}
+
+[${this.barAttribute}-scroll] {
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  padding: calc(var(--base-rhythm)*2);
+  padding-bottom: calc(var(--base-rhythm)*8);
+  scrollbar-color: transparent var(--scrollbarBG);
+  /* scrollbar-width: thin; */
+}
+[${this.barAttribute}-scroll='y'] {
+  overflow-x: hidden;
+  overflow-y: scroll;
+}
+[${this.barAttribute}-scroll]:hover {
+  scrollbar-color: var(--thumbBG) var(--scrollbarBG);
+}
+[${this.barAttribute}-scroll]::-webkit-scrollbar {
+  /* width: var(--base-rhythm); */
+}
+[${this.barAttribute}-scroll]::-webkit-scrollbar-thumb {
+  background-color: transparent;
+}
+[${this.barAttribute}-scroll]:hover::-webkit-scrollbar-thumb {
+  background-color: var(--thumbBG)
+}
+
+[data-state="closed"] [${this.barAttribute}-scroll] {
+  display: none;
+}
+    `;
   }
 
   _updateCurrents(e) {
@@ -433,11 +437,7 @@ class ResizableBar {
   }
 }
 
-const resizableBar = new ResizableBar({
-  barAttribute: BAR_ATTRIBUTE,
-  barStyle: STYLE,
-  barGravity: BAR_MIN_WIDTH,
-});
+const resizableBar = new ResizableBar({});
 
 window.addEventListener("load", function () {
   resizableBar.init();

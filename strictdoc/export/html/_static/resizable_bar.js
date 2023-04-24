b26b8253d02786dfabc00bbdf21edab34ef1a143
Maryna Balioura <mettta@gmail.com>
2023-04-24 22:32:59 +0200

    export/html: resizable_bar.js: remove ⋮ symbol from preloader

--- strictdoc/export/html/_static/resizable_bar.js
+++ strictdoc/export/html/_static/resizable_bar.js
@@ -529,7 +529,7 @@ class ResizableBar {
         display: flex;
         align-items: center;
         justify-content: center;
-        content: '⋮';
+        content: '';
         position: absolute;
         left: 0; right: 0; top: 0; bottom: 0;
         background-color: ${this.barColorBackground};

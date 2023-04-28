bacbe6e340661cce038a7b77fbb017db8202c89b
Maryna Balioura <mettta@gmail.com>
2023-04-29 01:04:43 +0200

    export/html: pan_with_space.js: use scrollIntoView() for centring the main column

--- strictdoc/export/html/_static/pan_with_space.js
+++ strictdoc/export/html/_static/pan_with_space.js
@@ -19,122 +19,124 @@ window.addEventListener('load', function () {
   }
 
   const element = getPanElement();
-  console.assert(!!element, "Expected a valid element.");
+  if (element) {
+    console.assert(!!element, "Expected a valid element.");
 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === ' ' || e.key === 'Spacebar') {
-      // ' ' is standard, 'Spacebar' was used by IE9 and Firefox < 37
+    document.addEventListener("keydown", function (e) {
+      if (e.key === ' ' || e.key === 'Spacebar') {
+        // ' ' is standard, 'Spacebar' was used by IE9 and Firefox < 37
+
+        e.preventDefault();
+        e.stopPropagation();
+        state.spacePressed = true;
+        element.style.cursor = 'move';
+        element.style.scrollBehavior = 'auto';
+        return;
+      }
+
+      var moveFactor = KEYDOWN_SPEED_FACTOR;
+      if (e.altKey) {
+        moveFactor = KEYDOWN_SPEED_FACTOR * 10;
+      }
+
+      if (e.key === 'ArrowDown') {
+        e.preventDefault();
+        e.stopPropagation();
+        element.scrollTop = element.scrollTop + moveFactor;
+      }
+      else if (e.key === 'ArrowUp') {
+        e.preventDefault();
+        e.stopPropagation();
+        element.scrollTop = element.scrollTop - moveFactor;
+      }
+      else if (e.key === 'ArrowLeft') {
+        e.preventDefault();
+        e.stopPropagation();
+        element.scrollLeft = element.scrollLeft - moveFactor;
+      }
+      else if (e.key === 'ArrowRight') {
+        e.preventDefault();
+        e.stopPropagation();
+        element.scrollLeft = element.scrollLeft + moveFactor;
+      }
+    })
+
+    document.addEventListener("keyup", function (e) {
+      if (e.key === ' ' || e.key === 'Spacebar') {
+        // ' ' is standard, 'Spacebar' was used by IE9 and Firefox < 37
+        e.preventDefault();
+        e.stopPropagation();
+        state.spacePressed = false;
+        element.style.cursor = 'default';
+        element.style.scrollBehavior = '';
+      }
+    });
+
+    element.addEventListener("mousedown", function (e) {
+      if (!state.spacePressed) {
+        return;
+      }
+
+      // Tell the browser we're handling this event.
+      e.preventDefault();
+      e.stopPropagation();
+
+      // Calc the starting mouse X,Y for the drag.
+      state.startX = parseInt(e.clientX);
+      state.startY = parseInt(e.clientY);
+
+      var mouseX = parseInt(e.clientX);
+      var mouseY = parseInt(e.clientY);
+
+      state.isDown = true;
+    });
+
+    element.addEventListener("mouseup", function (e) {
+      if (!state.spacePressed) {
+        return;
+      }
 
       e.preventDefault();
       e.stopPropagation();
-      state.spacePressed = true;
-      element.style.cursor = 'move';
-      element.style.scrollBehavior = 'auto';
-      return;
-    }
 
-    var moveFactor = KEYDOWN_SPEED_FACTOR;
-    if (e.altKey) {
-      moveFactor = KEYDOWN_SPEED_FACTOR * 10;
-    }
+      state.isDown = false;
+    });
 
-    if (e.key === 'ArrowDown') {
+    element.addEventListener("mousemove", function (e) {
+      if (!state.isDown) {
+        return;
+      }
+
+      if (!state.spacePressed) {
+        return;
+      }
+
       e.preventDefault();
       e.stopPropagation();
-      element.scrollTop = element.scrollTop + moveFactor;
-    }
-    else if (e.key === 'ArrowUp') {
+
+      var mouseX = parseInt(e.clientX);
+      var mouseY = parseInt(e.clientY);
+
+      var dx = mouseX - state.startX;
+      var dy = mouseY - state.startY;
+
+      state.startX = mouseX;
+      state.startY = mouseY;
+
+      var speedupFactor = MOUSEMOVE_SPEED_FACTOR;
+      element.scrollTo(
+        element.scrollLeft - dx * speedupFactor, element.scrollTop - dy * speedupFactor
+      );
+    });
+
+    element.addEventListener("mouseleave", function (e) {
+      // Tell the browser we're handling this event.
       e.preventDefault();
       e.stopPropagation();
-      element.scrollTop = element.scrollTop - moveFactor;
-    }
-    else if (e.key === 'ArrowLeft') {
-      e.preventDefault();
-      e.stopPropagation();
-      element.scrollLeft = element.scrollLeft - moveFactor;
-    }
-    else if (e.key === 'ArrowRight') {
-      e.preventDefault();
-      e.stopPropagation();
-      element.scrollLeft = element.scrollLeft + moveFactor;
-    }
-  })
 
-  document.addEventListener("keyup", function (e) {
-    if (e.key === ' ' || e.key === 'Spacebar') {
-      // ' ' is standard, 'Spacebar' was used by IE9 and Firefox < 37
-      e.preventDefault();
-      e.stopPropagation();
-      state.spacePressed = false;
-      element.style.cursor = 'default';
-      element.style.scrollBehavior = '';
-    }
-  });
-
-  element.addEventListener("mousedown", function (e) {
-    if (!state.spacePressed) {
-      return;
-    }
-
-    // Tell the browser we're handling this event.
-    e.preventDefault();
-    e.stopPropagation();
-
-    // Calc the starting mouse X,Y for the drag.
-    state.startX = parseInt(e.clientX);
-    state.startY = parseInt(e.clientY);
-
-    var mouseX = parseInt(e.clientX);
-    var mouseY = parseInt(e.clientY);
-
-    state.isDown = true;
-  });
-
-  element.addEventListener("mouseup", function (e) {
-    if (!state.spacePressed) {
-      return;
-    }
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    state.isDown = false;
-  });
-
-  element.addEventListener("mousemove", function (e) {
-    if (!state.isDown) {
-      return;
-    }
-
-    if (!state.spacePressed) {
-      return;
-    }
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    var mouseX = parseInt(e.clientX);
-    var mouseY = parseInt(e.clientY);
-
-    var dx = mouseX - state.startX;
-    var dy = mouseY - state.startY;
-
-    state.startX = mouseX;
-    state.startY = mouseY;
-
-    var speedupFactor = MOUSEMOVE_SPEED_FACTOR;
-    element.scrollTo(
-      element.scrollLeft - dx * speedupFactor, element.scrollTop - dy * speedupFactor
-    );
-  });
-
-  element.addEventListener("mouseleave", function (e) {
-    // Tell the browser we're handling this event.
-    e.preventDefault();
-    e.stopPropagation();
-
-    state.isDown = false;
-  });
+      state.isDown = false;
+    });
+  }
 });
 
 // When a window is loaded, scroll to the central column of the DTR tree.
@@ -142,19 +144,8 @@ window.addEventListener('load', function () {
 // listed top-to-bottom.
 document.addEventListener("DOMContentLoaded", function(event) {
   const element = getPanElement();
-  const firstNode = document.querySelector('.content_item[data-role="current"]');
-
-  // If there is content:
-  if (firstNode) {
-    const elementViewportOffset = element.getBoundingClientRect();
-    const firstNodeViewportOffset = firstNode.getBoundingClientRect();
-
-    const leftDiff = firstNodeViewportOffset.x - elementViewportOffset.x;
-
-    element.scrollTo(
-      leftDiff - window.screen.width / 2 + firstNode.offsetWidth / 2,
-      0,
-    );
-  } // otherwise we do nothing.
-
+  if (element) {
+    const firstNode = element.querySelector('.content_item[data-role="current"]');
+    firstNode && firstNode.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
+  }
 });

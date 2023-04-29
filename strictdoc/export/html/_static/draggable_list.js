759a4e0f7ea0ec610dd0d944ce0fad395a5013e9
Maryna Balioura <mettta@gmail.com>
2023-04-30 01:45:11 +0200

    export/html: init draggable_list

---
+++ strictdoc/export/html/_static/draggable_list.js
@@ -0,0 +1,6 @@
+const DL_SELECTOR = "[js-draggable_list]";
+
+window.addEventListener('load', function () {
+  const draggableList = document.querySelector(DL_SELECTOR);
+  console.log('draggable list is here', draggableList)
+})

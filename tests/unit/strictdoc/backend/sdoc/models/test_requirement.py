f7752ab8db4d4d6173dc6021d9284ecb773d89b7
Stanislav Pankevich <s.pankevich@gmail.com>
2023-07-08 22:34:12 +0200

    Code climate: requirement meta fields: refactor test_04_meta_multilines_not_nones test

--- tests/unit/strictdoc/backend/sdoc/models/test_requirement.py
+++ tests/unit/strictdoc/backend/sdoc/models/test_requirement.py
@@ -1,5 +1,9 @@
+from strictdoc.backend.sdoc.models.document import Document
+from strictdoc.backend.sdoc.models.document_config import DocumentConfig
 from strictdoc.backend.sdoc.models.document_grammar import DocumentGrammar
 from strictdoc.backend.sdoc.models.object_factory import SDocObjectFactory
+from strictdoc.backend.sdoc.models.requirement import RequirementField
+from strictdoc.backend.sdoc.models.type_system import GrammarElementFieldString
 
 
 def test_01_empty_requirement_as_starting_point():
@@ -51,3 +55,44 @@ def test_02_mutating_with_a_field():
         "STATEMENT",
         "RATIONALE",
     ]
+
+
+def test_04_meta_multilines_not_nones():
+    document_config = DocumentConfig.default_config(None)
+    document = Document("Test Doc", document_config, None, [], [])
+    grammar: DocumentGrammar = DocumentGrammar.create_default(document)
+
+    grammar.elements[0].fields.append(
+        GrammarElementFieldString(
+            parent=None, title="META_TEST_FIELD", required="False"
+        ),
+    )
+    document.grammar = grammar
+
+    requirement = SDocObjectFactory.create_requirement(
+        document,
+        requirement_type="REQUIREMENT",
+        title=None,
+        uid="A-1",
+        level=None,
+        statement=None,
+        statement_multiline="the bar must foo",
+        rationale=None,
+        rationale_multiline=None,
+        tags=None,
+        comments=None,
+    )
+
+    test_value = "a long\nmultiline value instead\nof the single line one"
+    requirement.ordered_fields_lookup["META_TEST_FIELD"] = [
+        RequirementField(
+            parent=None,
+            field_name="META_TEST_FIELD",
+            field_value=None,
+            field_value_multiline=test_value,
+            field_value_references=None,
+        )
+    ]
+    meta_fields = list(requirement.enumerate_meta_fields())
+    assert meta_fields[0][1] == "A-1"
+    assert meta_fields[1][1] == test_value

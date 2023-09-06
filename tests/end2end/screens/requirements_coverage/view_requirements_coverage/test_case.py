a4e8812812c23002d3c4324701c7eced355f8451
Stanislav Pankevich <s.pankevich@gmail.com>
2023-09-06 18:57:24 +0200

    Code climate: update several e2e tests to latest conventions

--- tests/end2end/screens/requirements_coverage/view_requirements_coverage/test_case.py
+++ tests/end2end/screens/requirements_coverage/view_requirements_coverage/test_case.py
@@ -12,7 +12,7 @@ from tests.end2end.server import SDocTestServer
 path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
 
 
-class Test_UC200_T01_ViewRequirementsCoverage(E2ECase):
+class Test(E2ECase):
     def test(self):
         with SDocTestServer(
             input_path=path_to_this_test_file_folder

8ae4e83658419a6d5af0777cb10aeaf6392b92ed
Stanislav Pankevich <s.pankevich@gmail.com>
2023-10-10 21:13:22 +0200

    tests/unit: test_update_requirement: double-check removing child relations

--- tests/unit/strictdoc/core/transforms/test_update_requirement.py
+++ tests/unit/strictdoc/core/transforms/test_update_requirement.py
@@ -10,7 +10,7 @@ from strictdoc.helpers.mid import MID
 from tests.unit.helpers.test_document_builder import DocumentBuilder
 
 
-def test_01_adding_a_second_parent_relation_with_role():
+def test_01_single_document_add_second_parent_relation_with_role():
     document_builder = DocumentBuilder()
     requirement1 = document_builder.add_requirement("REQ-001")
     requirement2 = document_builder.add_requirement("REQ-002")
@@ -75,7 +75,7 @@ def test_01_adding_a_second_parent_relation_with_role():
     ]
 
 
-def test_20_adding_a_second_child_relation_with_role():
+def test_20_single_document_add_second_child_relation_with_role():
     document_builder = DocumentBuilder()
     requirement1 = document_builder.add_requirement("REQ-001")
     requirement2 = document_builder.add_requirement("REQ-002")
@@ -146,3 +146,141 @@ def test_20_adding_a_second_child_relation_with_role():
         (requirement1, "IsRefinedBy"),
         (requirement1, "IsImplementedBy"),
     ]
+
+
+def test_25_single_document_remove_child_relation():
+    document_builder = DocumentBuilder()
+    requirement1 = document_builder.add_requirement("REQ-001")
+    requirement2 = document_builder.add_requirement("REQ-002")
+    document_builder.add_requirement_relation(
+        relation_type="Child",
+        source_requirement_id="REQ-002",
+        target_requirement_id="REQ-001",
+        role="IsRefinedBy",
+    )
+
+    assert len(requirement2.references) == 1
+
+    document_1 = document_builder.build()
+
+    file_tree = []
+    document_list = [document_1]
+    map_docs_by_paths = {}
+    document_tree = DocumentTree(
+        file_tree=file_tree,
+        document_list=document_list,
+        map_docs_by_paths=map_docs_by_paths,
+        map_docs_by_rel_paths={},
+    )
+    traceability_index: TraceabilityIndex = (
+        TraceabilityIndexBuilder.create_from_document_tree(document_tree)
+    )
+    traceability_index.document_tree = document_tree
+    requirement1_parents = list(
+        traceability_index.get_parent_relations_with_roles(requirement1)
+    )
+    assert requirement1_parents == [(requirement2, "IsRefinedBy")]
+
+    requirement2_children = list(
+        traceability_index.get_child_relations_with_roles(requirement2)
+    )
+    assert requirement2_children == [(requirement1, "IsRefinedBy")]
+
+    form_object: RequirementFormObject = (
+        RequirementFormObject.create_from_requirement(requirement=requirement2)
+    )
+
+    # Form object has no relations.
+    form_object.reference_fields.clear()
+
+    update_command = UpdateRequirementCommand(
+        form_object=form_object,
+        requirement=requirement2,
+        traceability_index=traceability_index,
+    )
+    update_command.perform()
+
+    requirement1_parents = list(
+        traceability_index.get_parent_relations_with_roles(requirement1)
+    )
+    assert requirement1_parents == []
+
+    requirement2_children = list(
+        traceability_index.get_child_relations_with_roles(requirement2)
+    )
+    assert requirement2_children == []
+
+
+def test_26_two_documents_remove_child_relation():
+    # Document 1
+    document_builder = DocumentBuilder()
+    requirement1 = document_builder.add_requirement("REQ-001")
+    document_1 = document_builder.build()
+
+    # Document 2
+    document_builder = DocumentBuilder()
+    requirement2 = document_builder.add_requirement("REQ-002")
+    document_builder.add_requirement_relation(
+        relation_type="Child",
+        source_requirement_id="REQ-002",
+        target_requirement_id="REQ-001",
+        role="IsRefinedBy",
+    )
+    assert len(requirement2.references) == 1
+    document_2 = document_builder.build()
+
+    file_tree = []
+    document_list = [document_1, document_2]
+    map_docs_by_paths = {}
+    document_tree = DocumentTree(
+        file_tree=file_tree,
+        document_list=document_list,
+        map_docs_by_paths=map_docs_by_paths,
+        map_docs_by_rel_paths={},
+    )
+    traceability_index: TraceabilityIndex = (
+        TraceabilityIndexBuilder.create_from_document_tree(document_tree)
+    )
+    traceability_index.document_tree = document_tree
+
+    assert traceability_index.get_document_children(document_1) == set()
+    assert traceability_index.get_document_parents(document_2) == set()
+    assert traceability_index.get_document_parents(document_1) == {
+        document_2
+    }
+    assert traceability_index.get_document_children(document_2) == {
+        document_1
+    }
+
+    requirement2_children = list(
+        traceability_index.get_child_relations_with_roles(requirement2)
+    )
+    assert requirement2_children == [(requirement1, "IsRefinedBy")]
+
+    form_object: RequirementFormObject = (
+        RequirementFormObject.create_from_requirement(requirement=requirement2)
+    )
+
+    # Form object has no relations.
+    form_object.reference_fields.clear()
+
+    update_command = UpdateRequirementCommand(
+        form_object=form_object,
+        requirement=requirement2,
+        traceability_index=traceability_index,
+    )
+    update_command.perform()
+
+    requirement1_parents = list(
+        traceability_index.get_parent_relations_with_roles(requirement1)
+    )
+    assert requirement1_parents == []
+
+    requirement2_children = list(
+        traceability_index.get_child_relations_with_roles(requirement2)
+    )
+    assert requirement2_children == []
+    assert traceability_index.get_document_children(document_1) == set()
+    assert traceability_index.get_document_children(document_2) == set()
+    assert traceability_index.get_document_parents(document_1) == set()
+    assert traceability_index.get_document_parents(document_2) == set()

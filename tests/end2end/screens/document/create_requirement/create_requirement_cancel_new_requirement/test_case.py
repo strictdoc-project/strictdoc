c9ce12183a1e46dbc98008156e5ad2c7c48e9ead
Stanislav Pankevich <s.pankevich@gmail.com>
2023-10-14 16:01:12 +0200

    Code climate: update a few end2end tests to latest conventions

--- tests/end2end/screens/document/create_requirement/create_requirement_cancel_new_requirement/test_case.py
+++ tests/end2end/screens/document/create_requirement/create_requirement_cancel_new_requirement/test_case.py
@@ -9,7 +9,7 @@ from tests.end2end.helpers.screens.project_index.screen_project_index import (
 from tests.end2end.server import SDocTestServer
 
 
-class Test_UC06_T01_CancelNewRequirement(E2ECase):
+class Test(E2ECase):
     def test(self):
         test_setup = End2EndTestSetup(path_to_test_file=__file__)
 

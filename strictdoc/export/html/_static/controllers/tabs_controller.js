e87d5f5b40b8c5250472c40bb4e634781b7ef5ba
Maryna Balioura <mettta@gmail.com>
2023-10-17 22:23:20 +0200

    export/html: error handling in different tabs

--- strictdoc/export/html/_static/controllers/tabs_controller.js
+++ strictdoc/export/html/_static/controllers/tabs_controller.js
@@ -1,6 +1,11 @@
 Stimulus.register("tabs", class extends Controller {
   initialize() {
 
+    const errors =[...this.element.querySelectorAll('sdoc-form-error')];
+    if(errors.length) {
+      // errors[0]
+    }
+
     // ** Tab buttons like this:
     // <sdoc-tabs>
     //  <sdoc-tab style="order: 0;" active>Id1</sdoc-tab>
@@ -8,41 +13,58 @@ Stimulus.register("tabs", class extends Controller {
     // </sdoc-tab>
     const sdocTabs = document.createElement('sdoc-tabs');
 
-    const tabs =[...this.element.querySelectorAll('sdoc-tab-content')]
+    const tabsData =[...this.element.querySelectorAll('sdoc-tab-content')]
     .reduce((accumulator, currentSdocTabContentElement, currentIndex) => {
-      // get data from the content element in DOM:
+      // get data from the contents of the tab element in the DOM:
       const key = currentSdocTabContentElement.id;
       const state = currentSdocTabContentElement.hasAttribute('active');
+
+      const errors =[...currentSdocTabContentElement.querySelectorAll('sdoc-form-error')];
 
       // create and prepare tab element:
       const sdocTab = document.createElement('sdoc-tab');
       sdocTab.style.order = currentIndex;
       sdocTab.innerHTML = key;
-      sdocTab.addEventListener("mouseup",() => {
-        // deactivate all tabs:
-        for (const {element, handler} of Object.values(tabs)) {
-          element.removeAttribute('active');
-          handler.removeAttribute('active');
-        }
-        // activate the tab:
-        tabs[key].element.setAttribute('active', '');
-        tabs[key].handler.setAttribute('active', '');
+      sdocTab.addEventListener("mouseup", () => {
+        this._activateTab(tabsData.tabs, key)
       });
       sdocTabs.append(sdocTab);
       sdocTab.setAttribute('data-testid', `form-tab-${key}`);
       state && sdocTab.setAttribute('active', '');
 
+      if(errors.length) {
+        sdocTab.setAttribute('data-errors', errors.length);
+        accumulator.errors.push(key);
+      }
+
       // update accumulator (form state):
-      accumulator[key] = {};
-      accumulator[key].element = currentSdocTabContentElement;
-      accumulator[key].order = currentIndex;
-      accumulator[key].state = state;
-      accumulator[key].handler = sdocTab;
+      accumulator.tabs[key] = {};
+      accumulator.tabs[key].element = currentSdocTabContentElement;
+      accumulator.tabs[key].order = currentIndex;
+      accumulator.tabs[key].state = state;
+      accumulator.tabs[key].handler = sdocTab;
+      accumulator.tabs[key].errors = errors.length;
 
       return accumulator;
-    }, {});
+    }, {
+      tabs:{},
+      errors:[]
+    });
 
     // put tab handlers to the DOM
     this.element.prepend(sdocTabs);
+
+    // activate first tab with errors
+    tabsData.errors.length && this._activateTab(tabsData.tabs, tabsData.errors[0]);
+  }
+
+  _activateTab(tabs, tabName) {
+    for (const {element, handler} of Object.values(tabs)) {
+      element.removeAttribute('active');
+      handler.removeAttribute('active');
+    }
+    // activate the tab:
+    tabs[tabName].element.setAttribute('active', '');
+    tabs[tabName].handler.setAttribute('active', '');
   }
 });

ae6778193ed3406ed5d7ee6fccc588555f08d6e0
Maryna Balioura <mettta@gmail.com>
2023-12-29 23:22:47 +0100

    export/html: toc_highlighting.js: updates the TOC highlighting when the tree or content changes

--- strictdoc/export/html/_static/toc_highlighting.js
+++ strictdoc/export/html/_static/toc_highlighting.js
@@ -1,55 +1,106 @@
+const TOC_FRAME_SELECTOR = 'turbo-frame#frame-toc';
+const TOC_ELEMENT_SELECTOR = 'a';
+const CONTENT_FRAME_SELECTOR = 'main';
+const CONTENT_ELEMENT_SELECTOR = 'sdoc-anchor';
 
+let tocHighlightingState = {
+  data: {},
+  links: null,
+  anchors: null,
+};
 
-// *** IntersectionObserver
-
-let data = {};
-
+window.addEventListener("hashchange", handleHashChange);
 window.addEventListener("load",function(){
 
-  const tocFrame = document.querySelector('#frame-toc');
-  const contentFrame = document.querySelector('main');
+  // * Frames are stable and we define them once.
+  const tocFrame = document.querySelector(TOC_FRAME_SELECTOR);
+  const contentFrame = document.querySelector(CONTENT_FRAME_SELECTOR);
 
-  const linkList = tocFrame.querySelectorAll('a');
-  const anchorList = contentFrame.querySelectorAll('sdoc-anchor');
+  // * Call for the first time.
+  highlightTOC(tocFrame, contentFrame);
 
-  linkList.forEach(a => {
-    const id = a.getAttribute('anchor');
-    data[id] = {
-      'a': a
+  // * Then we will refresh when the TOC tree is updated&
+  // * The content in the tocFrame frame will mutate:
+  const mutatingFrame = tocFrame;
+  new MutationObserver(function (mutationsList, observer) {
+    // * Use requestAnimationFrame to put highlightTOC
+    // * at the end of the event queue and to ensure
+    // * the code runs after all DOM changes have been applied.
+    requestAnimationFrame(() => {
+      highlightTOC(tocFrame, contentFrame);
+    })
+  }).observe(
+    mutatingFrame,
+    {
+      // * We're looking at an updatable frame (mutates its contents,
+      // * and we don't care what mutations were made inside):
+      childList: true,
+      // attributes: true,
+      // characterData: true,
+      // subtree: true
     }
-  });
-  anchorList.forEach(anchor => {
-    const id = anchor.id;
-    data[id] = {
-      'anchor': anchor,
-      ...data[id]
-    };
-    createObserver(anchor);
-  });
-
-  // console.log(data);
-
-  // *** handleHashChange
-
-  const handleHashChange = () => {
-    const hash = window.location.hash;
-    // console.log("Hash changed:", hash);
-    const match = hash.match(/#(.*)/);
-    const fragment = match ? match[1] : null;
-    // console.log("Fragment:", fragment);
-
-    linkList.forEach(a => {
-      a.removeAttribute('targeted');
-    });
-    data[fragment].a.setAttribute('targeted', '');
-  }
-
-  handleHashChange();
-  window.addEventListener("hashchange", handleHashChange);
+  );
 
 },false);
 
-function createObserver(observedElement) {
+function highlightTOC(tocFrame, contentFrame) {
+  tocHighlightingState = {
+    data: {},
+    links: null,
+    anchors: null,
+  };
+  processLinkList(tocFrame);
+  processAnchorList(contentFrame);
+  handleHashChange();
+}
+
+function processLinkList(tocFrame) {
+  // * Collects all links in the TOC
+  tocHighlightingState.links = null;
+  tocHighlightingState.links = tocFrame.querySelectorAll(TOC_ELEMENT_SELECTOR);
+  tocHighlightingState.links.forEach(link => {
+    const id = link.getAttribute('anchor');
+    tocHighlightingState.data[id] = {
+      'link': link,
+      ...tocHighlightingState.data[id]
+    }
+  });
+}
+function processAnchorList(contentFrame) {
+  // * Collects all anchors in the document
+  tocHighlightingState.anchors = null;
+  tocHighlightingState.anchors = contentFrame.querySelectorAll(CONTENT_ELEMENT_SELECTOR);
+  tocHighlightingState.anchors.forEach(anchor => {
+    const id = anchor.id;
+    tocHighlightingState.data[id] = {
+      'anchor': anchor,
+      ...tocHighlightingState.data[id]
+    };
+    // * Adds an observer for the position of the anchor
+    createIntersectObserver(anchor);
+  });
+}
+
+function handleHashChange() {
+  const hash = window.location.hash;
+  const match = hash.match(/#(.*)/);
+  const fragment = match ? match[1] : null;
+
+  tocHighlightingState.links.forEach(link => {
+    link.removeAttribute('targeted');
+  });
+  // * When updating the hash
+  // * and there's a fragment,
+  fragment
+    // * and the corresponding link-anchor pair is registered,
+    && tocHighlightingState.data[fragment]
+    // * highlight the corresponding link.
+    && tocHighlightingState.data[fragment].link.setAttribute('targeted', '');
+}
+
+function createIntersectObserver(observedElement) {
+  // * Adds an observer for the position of the anchor
+  // * relative to the viewport
   let observer;
 
   let options = {
@@ -63,12 +114,14 @@ function createObserver(observedElement) {
 
 function handleIntersect(entries, observer) {
   entries.forEach((entry) => {
+    const anchor = entry.target.id;
+    // * For anchors that go into the viewport,
     if (entry.intersectionRatio > 0) {
-      data[entry.target.id].a.setAttribute('intersected', '');
+      // * finds the corresponding links in the TOC and highlights them,
+      tocHighlightingState.data[anchor].link.setAttribute('intersected', '');
     } else {
-      data[entry.target.id].a.removeAttribute('intersected');
+      // * and cancels highlighting for the rest of the links.
+      tocHighlightingState.data[anchor].link.removeAttribute('intersected');
     }
   });
 }
-
-

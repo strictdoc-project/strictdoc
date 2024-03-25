cce6f397c54b093c3d84f0af623d8e762ac32f2b
Maryna Balioura <mettta@gmail.com>
2024-03-25 04:37:35 +0100

    export/html: implement collapsible_tree.js; enable it for Project-tree and Source file coverage tree

---
+++ strictdoc/export/html/_static/collapsible_tree.js
@@ -0,0 +1,30 @@
+const ROOT_SELECTOR = 'js-collapsible_tree';
+
+window.addEventListener("DOMContentLoaded", function(){
+
+  const tree = document.querySelector(`[${ROOT_SELECTOR}]`);
+  if (!tree) { return }
+
+  const summaries = tree.querySelectorAll('summary');
+
+  summaries.forEach(summary => {
+    summary.addEventListener('dblclick', () => {
+      const details = summary.parentElement;
+      if (details.nodeName !== 'DETAILS') {
+        console.warn('The DETAILS tag has an unexpected structure.');
+        return
+      }
+      const open = details.hasAttribute("open");
+
+      summaries.forEach(summary => {
+        const details = summary.parentElement;
+        if (open) {
+          details.removeAttribute("open")
+        } else (
+          details.setAttribute("open", "")
+        )
+      })
+    });
+  })
+
+},false);

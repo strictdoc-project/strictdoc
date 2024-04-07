e16ad17ec0479d940df3ec4b7285a9039368b71d
Stanislav Pankevich <s.pankevich@gmail.com>
2024-04-07 11:26:54 +0200

    Project Diff/Changelog: Ignore changes in included grammars for now
    Closes #1759

--- strictdoc/export/html/generators/view_objects/traceability_matrix_view_object.py
+++ strictdoc/export/html/generators/view_objects/traceability_matrix_view_object.py
@@ -1,15 +1,66 @@
-from typing import Dict, Optional
+from typing import Dict, List, Optional, Tuple
 
+from jinja2 import Environment
+
+from strictdoc import __version__
 from strictdoc.backend.sdoc.models.document import SDocDocument
 from strictdoc.backend.sdoc.models.document_grammar import DocumentGrammar
+from strictdoc.core.document_tree_iterator import DocumentTreeIterator
 from strictdoc.core.project_config import ProjectConfig
 from strictdoc.core.traceability_index import TraceabilityIndex
-from strictdoc.export.html.generators.view_objects.traceability_matrix_view_object import (
-    TraceabilityMatrixViewObject,
-)
 from strictdoc.export.html.html_templates import HTMLTemplates
 from strictdoc.export.html.renderers.link_renderer import LinkRenderer
 from strictdoc.export.html.renderers.markup_renderer import MarkupRenderer
+
+
+class TraceabilityMatrixViewObject:
+    def __init__(
+        self,
+        *,
+        traceability_index: TraceabilityIndex,
+        project_config: ProjectConfig,
+        link_renderer: LinkRenderer,
+        markup_renderer: MarkupRenderer,
+        known_relations_list: List[Tuple],
+    ):
+        self.traceability_index: TraceabilityIndex = traceability_index
+        self.project_config: ProjectConfig = project_config
+        self.link_renderer: LinkRenderer = link_renderer
+        self.markup_renderer: MarkupRenderer = markup_renderer
+        self.known_relations_list: List[Tuple] = known_relations_list
+        self.standalone: bool = False
+        self.document_tree_iterator: DocumentTreeIterator = (
+            DocumentTreeIterator(traceability_index.document_tree)
+        )
+        self.is_running_on_server: bool = project_config.is_running_on_server
+        self.strictdoc_version = __version__
+
+    def iterate_documents(self):
+        yield from filter(
+            lambda document_: not document_.document_is_included(),
+            self.traceability_index.document_tree.document_list,
+        )
+
+    def render_screen(self, jinja_environment: Environment):
+        template = jinja_environment.get_template(
+            "screens/traceability_matrix/index.jinja"
+        )
+        return template.render(view_object=self)
+
+    def render_static_url(self, url: str):
+        return self.link_renderer.render_static_url(url)
+
+    def render_url(self, url: str):
+        return self.link_renderer.render_url(url)
+
+    def render_static_url_with_prefix(self, url: str):
+        return self.link_renderer.render_static_url_with_prefix(url)
+
+    def render_local_anchor(self, node):
+        return self.link_renderer.render_local_anchor(node)
+
+    def is_empty_tree(self) -> bool:
+        return self.document_tree_iterator.is_empty_tree()
 
 
 class TraceabilityMatrixHTMLGenerator:

2cca5ced9bbe2503e430d03cdfdc4f5c9154b342
mattplum <matthew_plummer@selinc.com>
2025-01-28 08:54:21 -0800

    Report search query error message on UI

--- tests/end2end/helpers/screens/search/search.py
+++ tests/end2end/helpers/screens/search/search.py
@@ -1,3 +1,4 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from tests.end2end.helpers.screens.screen import Screen
@@ -8,6 +9,11 @@ class Screen_SearchResults(Screen):  # pylint: disable=invalid-name
         self.test_case.click_xpath(
             '//a[@data-testid="node.is_requirement"]',
         )
+
+    def get_search_error_msg(self):
+        return self.test_case.find_element(
+            By.XPATH, "//div[@class='sdoc-form-error']"
+        ).text
 
     def assert_nr_results(self, nr_results: int):
         content = (
@@ -31,5 +37,16 @@ class Screen_Search(Screen):  # pylint: disable=invalid-name
     def do_click_on_search_sections(self) -> Screen_SearchResults:
         self.test_case.click_xpath(
             '//a[@data-testid="node.is_section"]',
+        )
+        return Screen_SearchResults(self.test_case)
+
+    def do_search(self, query) -> Screen_SearchResults:
+        textBox = self.test_case.driver.find_element(
+            By.XPATH, "//input[@id='q']"
+        )
+        textBox.clear()
+        textBox.send_keys(query)
+        self.test_case.click_xpath(
+            '//button[@data-testid="form-submit-action"]',
         )
         return Screen_SearchResults(self.test_case)

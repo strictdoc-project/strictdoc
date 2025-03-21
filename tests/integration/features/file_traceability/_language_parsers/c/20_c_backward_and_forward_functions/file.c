95bd26681d20d1c19405302612addfe9100aa67f
Maryna Balioura <mettta@gmail.com>
2025-03-21 17:55:01 +0100

    tests/integration: source view test: add more test content

--- tests/integration/features/file_traceability/_language_parsers/c/20_c_backward_and_forward_functions/file.c
+++ tests/integration/features/file_traceability/_language_parsers/c/20_c_backward_and_forward_functions/file.c
@@ -1,12 +1,49 @@
 /**
- * @relation(REQ-2, REQ-3, scope=function)
- *
+ * @relation(REQ-2, scope=function)
+ * @relation(REQ-1, scope=function)
  */
 void foo() {
-
+  // foo
+  // comment comment comment comment comment comment comment comment comment comment comment
 }
 
-// @relation(REQ-2, REQ-3, scope=function)
+// @relation(REQ-3, scope=range_start)
 void bar() {
+  // bar
+}
+// @relation(REQ-3, scope=range_end)
 
+// @relation(REQ-3, scope=function)
+void boo() {
+  // boo
+  // boo
+  // boo
+  // boo
+  // @relation(REQ-4, scope=range_start)
+  // boo
+  // boo
+  // boo
+  // boo // @relation(REQ-1, scope=line)
+  // boo
+  // boo
+  // @relation(REQ-4, scope=range_end)
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
+  // boo
 }

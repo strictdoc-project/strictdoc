80b297e66bc9cabb048869f4072947cda4d6ffaf
Stanislav Pankevich <s.pankevich@gmail.com>
2025-04-24 22:23:07 +0200

    Code climate: helpers/math: add basic unit test

--- strictdoc/helpers/math.py
+++ strictdoc/helpers/math.py
@@ -6,8 +6,6 @@ def round_up(number: float, decimals):
     assert isinstance(number, float)
     assert isinstance(decimals, int)
     assert decimals >= 0
-    if decimals == 0:
-        return ceil(number)
 
     factor = 10**decimals
 

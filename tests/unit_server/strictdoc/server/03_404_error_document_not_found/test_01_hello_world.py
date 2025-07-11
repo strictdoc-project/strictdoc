364e357524e842d496cb34b78ab9459ece909751
Stanislav Pankevich <s.pankevich@gmail.com>
2025-07-12 00:51:47 +0200

    tests/unit_server: main_router: add missing coverage for non-activated features

--- tests/unit_server/strictdoc/server/03_404_error_document_not_found/test_01_hello_world.py
+++ tests/unit_server/strictdoc/server/03_404_error_document_not_found/test_01_hello_world.py
@@ -1,5 +1,6 @@
 import os
 
+import pytest
 from fastapi.testclient import TestClient
 
 from strictdoc import environment
@@ -10,7 +11,8 @@ from strictdoc.server.app import create_app
 PATH_TO_THIS_TEST_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_get_document():
+@pytest.fixture(scope="module")
+def project_config():
     server_config = ServerCommandConfig(
         input_path=PATH_TO_THIS_TEST_FOLDER,
         output_path=os.path.join(PATH_TO_THIS_TEST_FOLDER, "output"),
@@ -23,6 +25,9 @@ def test_get_document():
         environment=environment
     )
     project_config.integrate_server_config(server_config)
+    return project_config
+
+def test_get_non_existing_document(project_config: ProjectConfig):
     client = TestClient(
         create_app(
             project_config=project_config
@@ -30,3 +35,24 @@ def test_get_document():
     )
     response = client.get("/does_not_exist/sample.html")
     assert response.status_code == 404
+
+def test_non_default_features_when_not_activated(project_config: ProjectConfig):
+    client = TestClient(
+        create_app(
+            project_config=project_config
+        )
+    )
+    response = client.get("/traceability_matrix.html")
+    assert response.status_code == 412
+
+    response = client.get("/source_coverage.html")
+    assert response.status_code == 412
+
+    response = client.get("/project_statistics.html")
+    assert response.status_code == 412
+
+    response = client.get("/some_document-PDF.html")
+    assert response.status_code == 412
+
+    response = client.get("/some_document.standalone.html")
+    assert response.status_code == 412

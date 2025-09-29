9aa2fa5845fb706fa41c0bfe46fee326727723dd
Stanislav Pankevich <s.pankevich@gmail.com>
2025-09-29 19:47:36 +0200

    chore(tests/end2end): create_section_two_sibling_sections: remove legacy Section

--- tests/end2end/screens/document/create_section/create_section_two_sibling_sections/test_case.py
+++ tests/end2end/screens/document/create_section/create_section_two_sibling_sections/test_case.py
@@ -1,7 +1,7 @@
 from tests.end2end.e2e_case import E2ECase
 from tests.end2end.end2end_test_setup import End2EndTestSetup
-from tests.end2end.helpers.screens.document.form_edit_section import (
-    Form_EditSection,
+from tests.end2end.helpers.screens.document.form_edit_requirement import (
+    Form_EditRequirement,
 )
 from tests.end2end.helpers.screens.project_index.screen_project_index import (
     Screen_ProjectIndex,
@@ -35,16 +35,16 @@ class Test(E2ECase):
 
             root_node = screen_document.get_root_node()
             root_node_menu = root_node.do_open_node_menu()
-            form_edit_section_1: Form_EditSection = (
-                root_node_menu.do_node_add_section_first()
+            form_edit_section_1: Form_EditRequirement = (
+                root_node_menu.do_node_add_element_first("SECTION")
             )
 
-            form_edit_section_1.do_fill_in_title("First title")
+            form_edit_section_1.do_fill_in("TITLE", "First title")
             form_edit_section_1.do_form_submit()
 
-            section_1 = screen_document.get_section()
+            section_1 = screen_document.get_node(1)
 
-            section_1.assert_section_title("First title", "1")
+            section_1.assert_requirement_title("First title", "1")
             screen_document.assert_toc_contains("First title")
 
             #
@@ -53,15 +53,15 @@ class Test(E2ECase):
 
             section_1_node_menu = section_1.do_open_node_menu()
 
-            form_edit_section2: Form_EditSection = (
-                section_1_node_menu.do_node_add_section_below()
+            form_edit_section2: Form_EditRequirement = (
+                section_1_node_menu.do_node_add_element_below("SECTION")
             )
-            form_edit_section2.do_fill_in_title("Second title")
+            form_edit_section2.do_fill_in("TITLE", "Second title")
             form_edit_section2.do_form_submit()
 
-            section2 = screen_document.get_section(2)
+            section2 = screen_document.get_node(2)
 
-            section2.assert_section_title("Second title", "2")
+            section2.assert_requirement_title("Second title", "2")
             screen_document.assert_toc_contains("Second title")
 
         assert test_setup.compare_sandbox_and_expected_output()

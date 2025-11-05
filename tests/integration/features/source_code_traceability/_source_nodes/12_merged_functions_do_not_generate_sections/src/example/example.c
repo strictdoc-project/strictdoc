1f3f9727c99e5362bc2fb5f8d0fd2cdde7fd3efd
Tobias Deiminger <tobias.deiminger@linutronix.de>
2025-11-05 22:33:06 +0100

    fix(backend/sdoc_source_code): Don't auto-generate sections for merged source nodes
    Until now, StrictDoc unconditionally created a section for each file
    path component of a source node. This is however not needed in cases
    where the source node is merged into a static node, as the static node
    already has a static parent section. The auto-generated section would
    appear as unexpected empty reminder, so don't generate them in such a
    case.

---
+++ tests/integration/features/source_code_traceability/_source_nodes/12_merged_functions_do_not_generate_sections/src/example/example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Some text.
+ *
+ * UID: REQ-EXAMPLE-1
+ *
+ * TITLE: Title from example.c
+ *
+ * STATEMENT: Statement from example.c
+ */
+void example_1(void) {
+    print("hello world\n");
+}

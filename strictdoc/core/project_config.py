strictdoc-project/strictdoc

commit ce2c02e7b46a1668b0397ed74ea7e22e082d120a

feat(project_config): simplify API for use outside StrictDoc CLI
WHAT:

Encapsulate `validate_and_finalize()` inside ProjectConfigLoader. Introduce dedicated `load...` methods on the ProjectConfigLoader class that handle merging the project config with command-line config objects.

As part of this work, the server command now generates output files in `./output/server` by default. Previously, the output folder was created in a temporary location if not specified by the user.

WHY:

This improves encapsulation of how project and CLI command configs are merged, reducing the risk of undefined behavior when the project config is not initialized correctly, especially when the StrictDoc Python API is used outside the standard CLI workflow.

This also fixes a user report where undefined behavior occurred due to a missing `validate_and_finalize()` call. The affected Python API example (export questionnaire) has been updated to use the new, better-encapsulated API.

Closes #2663

strictdoc/core/project_config.py
@@ -18,6 +18,9 @@ from strictdoc import __version__, environment
 from strictdoc.backend.reqif.sdoc_reqif_fields import ReqIFProfile
 from strictdoc.backend.sdoc.constants import SDocMarkup
 from strictdoc.commands.export_config import ExportCommandConfig
+from strictdoc.commands.import_excel_config import ImportExcelCommandConfig
+from strictdoc.commands.import_reqif_config import ImportReqIFCommandConfig
+from strictdoc.commands.manage_autouid_config import ManageAutoUIDCommandConfig
 from strictdoc.commands.server_config import ServerCommandConfig
 from strictdoc.core.environment import SDocRuntimeEnvironment
 from strictdoc.core.plugin import StrictDocPlugin
@@ -408,17 +411,21 @@ class ProjectConfig:
             source_root_path = source_root_path.rstrip("/")
             self.source_root_path = source_root_path
 
-        if (output_dir_ := server_config.output_path) is not None:
-            self.output_dir = output_dir_
-            self.export_output_html_root = os.path.join(output_dir_, "html")
-            # If a custom cache folder is not specified in the config, adjust the
-            # cache folder to be located in the output folder.
-            if self.dir_for_sdoc_cache.startswith(
-                ProjectConfigDefault.DEFAULT_DIR_FOR_SDOC_CACHE
-            ):
-                self.dir_for_sdoc_cache = os.path.join(
-                    output_dir_, "_cache", __version__
-                )
+        output_dir = (
+            server_config.output_path or self.output_dir or "./output/server"
+        )
+
+        self.output_dir = output_dir
+        self.export_output_html_root = os.path.join(output_dir, "html")
+
+        # If a custom cache folder is not specified in the config, adjust the
+        # cache folder to be located in the output folder.
+        if self.dir_for_sdoc_cache.startswith(
+            ProjectConfigDefault.DEFAULT_DIR_FOR_SDOC_CACHE
+        ):
+            self.dir_for_sdoc_cache = os.path.join(
+                output_dir, "_cache", __version__
+            )
 
         self.export_formats = ["html"]
         self.generate_bundle_document = False
@@ -717,6 +724,101 @@ class ProjectConfig:
 
 
 class ProjectConfigLoader:
+    @classmethod
+    def load(
+        cls, input_path: str, output_dir: Optional[str] = None
+    ) -> ProjectConfig:
+        assert os.path.exists(input_path), input_path
+        project_config: ProjectConfig = cls.load_from_path_or_get_default(
+            path_to_config=input_path
+        )
+        project_config.input_paths = [input_path]
+        if output_dir is not None:
+            project_config.output_dir = output_dir
+        project_config.validate_and_finalize()
+        return project_config
+
+    @classmethod
+    def load_using_export_config(
+        cls,
+        export_config: ExportCommandConfig,
+    ) -> ProjectConfig:
+        path_to_config = export_config.get_path_to_config()
+        project_config: ProjectConfig = cls.load_from_path_or_get_default(
+            path_to_config=path_to_config
+        )
+        project_config.integrate_export_config(export_config)
+        project_config.validate_and_finalize()
+        return project_config
+
+    @classmethod
+    def load_using_server_config(
+        cls,
+        server_config: ServerCommandConfig,
+    ) -> ProjectConfig:
+        path_to_config = server_config.get_path_to_config()
+        project_config: ProjectConfig = cls.load_from_path_or_get_default(
+            path_to_config=path_to_config
+        )
+        project_config.integrate_server_config(server_config)
+        project_config.validate_and_finalize()
+        return project_config
+
+    @classmethod
+    def load_using_import_excel_config(
+        cls,
+        _: ImportExcelCommandConfig,
+    ) -> ProjectConfig:
+        path_to_config = os.getcwd()
+        project_config: ProjectConfig = cls.load_from_path_or_get_default(
+            path_to_config=path_to_config
+        )
+        project_config.input_paths = [path_to_config]
+        project_config.validate_and_finalize()
+        return project_config
+
+    @classmethod
+    def load_using_import_reqif_config(
+        cls,
+        _: ImportReqIFCommandConfig,
+    ) -> ProjectConfig:
+        path_to_config = os.getcwd()
+        project_config: ProjectConfig = cls.load_from_path_or_get_default(
+            path_to_config=path_to_config
+        )
+        project_config.input_paths = [path_to_config]
+        project_config.validate_and_finalize()
+        return project_config
+
+    @classmethod
+    def load_using_manage_autouid_config(
+        cls,
+        manage_autouid_config: ManageAutoUIDCommandConfig,
+    ) -> ProjectConfig:
+        path_to_config = manage_autouid_config.get_path_to_config()
+
+        project_config: ProjectConfig = cls.load_from_path_or_get_default(
+            path_to_config=path_to_config
+        )
+
+        # FIXME: Encapsulate all this in project_config.integrate_manage_autouid_config(),
+        #        following the example of integrate_export_config().
+        project_config.input_paths = [manage_autouid_config.input_path]
+        project_config.source_root_path = str(
+            Path(manage_autouid_config.input_path).resolve()
+        )
+        project_config.auto_uid_mode = True
+        project_config.autouuid_include_sections = (
+            manage_autouid_config.include_sections
+        )
+
+        # FIXME: Traceability Index is coupled with HTML output.
+        project_config.export_output_html_root = "NOT_RELEVANT"
+
+        project_config.validate_and_finalize()
+
+        return project_config
+
     @staticmethod
     def load_from_path_or_get_default(
         *,

strictdoc/commands/export.py
@@ -193,12 +193,9 @@ class ExportCommand(BaseCommand):
             export_config.validate()
         except CLIValidationError as exception_:
             raise exception_
-        project_config = ProjectConfigLoader.load_from_path_or_get_default(
-            path_to_config=export_config.get_path_to_config(),
+        project_config = ProjectConfigLoader.load_using_export_config(
+            export_config
         )
-        project_config.integrate_export_config(export_config)
-        project_config.validate_and_finalize()
-
         parallelization_value = (
             "Disabled" if export_config.no_parallelization else "Enabled"
         )

strictdoc/commands/import_excel.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 
 from strictdoc.cli.base_command import BaseCommand
 from strictdoc.commands.import_excel_config import ImportExcelCommandConfig
@@ -50,9 +49,8 @@ class ImportExcelCommand(BaseCommand):
         )
 
     def run(self, parallelizer: Parallelizer) -> None:  # noqa: ARG002
-        project_config = ProjectConfigLoader.load_from_path_or_get_default(
-            path_to_config=os.getcwd(),
+        project_config = ProjectConfigLoader.load_using_import_excel_config(
+            self.config
         )
-
         import_action = ImportAction()
         import_action.do_import(self.config, project_config)

strictdoc/commands/import_reqif.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 from typing import Optional
 
 from strictdoc.backend.reqif.sdoc_reqif_fields import ReqIFProfile
@@ -72,9 +71,8 @@ class ImportReqIFCommand(BaseCommand):
         )
 
     def run(self, parallelizer: Parallelizer) -> None:  # noqa: ARG002
-        project_config = ProjectConfigLoader.load_from_path_or_get_default(
-            path_to_config=os.getcwd(),
+        project_config = ProjectConfigLoader.load_using_import_reqif_config(
+            self.config
         )
-
         import_action = ImportAction()
         import_action.do_import(self.config, project_config)

strictdoc/commands/manage_autouid_command.py
@@ -1,6 +1,5 @@
 import argparse
 import sys
-from pathlib import Path
 from typing import Dict, Optional
 
 from strictdoc.backend.sdoc.errors.document_tree_error import DocumentTreeError
@@ -95,24 +94,9 @@ the document's PREFIX (if provided or "REQ-" by default).
         except CLIValidationError as exception_:
             raise exception_
 
-        project_config = ProjectConfigLoader.load_from_path_or_get_default(
-            path_to_config=manage_config.get_path_to_config(),
+        project_config = ProjectConfigLoader.load_using_manage_autouid_config(
+            manage_config
         )
-
-        # FIXME: Encapsulate all this in project_config.integrate_manage_autouid_config(),
-        #        following the example of integrate_export_config().
-        project_config.input_paths = [manage_config.input_path]
-        project_config.source_root_path = str(
-            Path(manage_config.input_path).resolve()
-        )
-        project_config.auto_uid_mode = True
-        project_config.autouuid_include_sections = (
-            manage_config.include_sections
-        )
-        project_config.validate_and_finalize()
-
-        # FIXME: Traceability Index is coupled with HTML output.
-        project_config.export_output_html_root = "NOT_RELEVANT"
 
         try:
             traceability_index: TraceabilityIndex = (

strictdoc/commands/server.py
@@ -52,8 +52,8 @@ class ServerCommand(BaseCommand):
             server_config.validate()
         except CLIValidationError as exception_:
             raise exception_
-        project_config = ProjectConfigLoader.load_from_path_or_get_default(
-            path_to_config=server_config.get_path_to_config(),
+        project_config = ProjectConfigLoader.load_using_server_config(
+            server_config
         )
         run_strictdoc_server(
             server_config=server_config, project_config=project_config

strictdoc/server/server.py
@@ -39,14 +39,6 @@ def run_strictdoc_server(
     print_warning_message()
 
     with ExitStack() as stack:
-        if server_config.output_path is None:
-            server_config.output_path = stack.enter_context(
-                tempfile.TemporaryDirectory()
-            )
-
-        project_config.integrate_server_config(server_config)
-        project_config.validate_and_finalize()
-
         reload_config = UvicornReloadConfig.create(
             project_config, server_config
         )

tasks.py
@@ -114,7 +114,6 @@ def server(context, input_path=".", config=None):
                 --debug
                 server {input_path} {config_argument}
                     --host 127.0.0.1
-                    --output-path ./output/server
                     --reload
         """,
     )

commit bc0b234f174ae2e882f17c4b982b5caf55b95005

fix(server): set default server path to output/server
WHAT:

This fixes an issue introduced in
ce2c02e7b46a1668b0397ed74ea7e22e082d120a where the server path was assumed to
be set to "output/server" but it was missed that the actual value was set to
"output" which causes collisions with static export output files.

WHY:

Without this change, the output dirs for static and server outputs are the same,
and this causes conflicts and undefined behavior when testing both workflows
at the same time.

strictdoc/core/project_config.py
@@ -86,6 +86,7 @@ class ProjectFeature(str, Enum):
 class ProjectConfigDefault:
     DEFAULT_PROJECT_TITLE = "Untitled Project"
     DEFAULT_DIR_FOR_SDOC_ASSETS = "_static"
+    DEFAULT_DIR_FOR_OUTPUT = "output"
     DEFAULT_DIR_FOR_SDOC_CACHE = "output/_cache"
 
     DEFAULT_FEATURES: List[str] = [
@@ -300,7 +301,7 @@ class ProjectConfig:
         # Settings derived from the command-line parameters.
 
         # Common settings.
-        self.output_dir: str = "output"
+        self.output_dir: str = ProjectConfigDefault.DEFAULT_DIR_FOR_OUTPUT
 
         # Export action.
         self.export_output_html_root: str = os.path.join(
@@ -411,11 +412,14 @@ class ProjectConfig:
             source_root_path = source_root_path.rstrip("/")
             self.source_root_path = source_root_path
 
-        output_dir = (
-            server_config.output_path or self.output_dir or "./output/server"
-        )
-
+        # When setting the output dir, the CLI argument takes precedence.
+        output_dir = self.output_dir
+        if server_config.output_path is not None:
+            output_dir = server_config.output_path
+        elif output_dir == ProjectConfigDefault.DEFAULT_DIR_FOR_OUTPUT:
+            output_dir = "./output/server"
         self.output_dir = output_dir
+
         self.export_output_html_root = os.path.join(output_dir, "html")
 
         # If a custom cache folder is not specified in the config, adjust the
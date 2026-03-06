0084fc4f303b8251058e2d356a77603a84ff5bad
Maryna Balioura <mettta@gmail.com>
2026-03-06 02:04:39 +0100

    feat(tests/e2e): add cases and helpers for TOC bulk actions in panel, branch bulk w/key, one-step undo

--- tests/end2end/helpers/components/collapsible_list.py
+++ tests/end2end/helpers/components/collapsible_list.py
@@ -1,4 +1,6 @@
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from seleniumbase import BaseCase
 
 
@@ -24,6 +26,24 @@ class CollapsibleList:
     def assert_collapsible_handler(self) -> None:
         self.test_case.assert_element(
             "//*[@data-handler]",
+            by=By.XPATH,
+        )
+
+    def assert_bulk_panel(self) -> None:
+        self.test_case.assert_element(
+            "//*[@js-collapsible_list-bulk_controls]",
+            by=By.XPATH,
+        )
+
+    def assert_bulk_button_collapse_all(self) -> None:
+        self.test_case.assert_element(
+            '//*[@js-collapsible_list-bulk_controls]//*[@data-bulk="collapse"]',
+            by=By.XPATH,
+        )
+
+    def assert_bulk_button_expand_all(self) -> None:
+        self.test_case.assert_element(
+            '//*[@js-collapsible_list-bulk_controls]//*[@data-bulk="expand"]',
             by=By.XPATH,
         )
 
@@ -85,5 +105,80 @@ class CollapsibleList:
     def do_bulk_expand_branch(self, string: str) -> None:
         self.test_case.double_click(
             f'//a[contains(., "{string}")]/../*[@data-handler="collapsed"]',
+            by=By.XPATH,
+        )
+
+    def do_bulk_collapse_branch_with_key(self, string: str) -> None:
+        # bulk collapse by SHIFT + click.
+        element = self.test_case.find_element(
+            f'//a[contains(., "{string}")]/../*[@data-handler="expanded"]',
+            by=By.XPATH,
+        )
+        ActionChains(self.test_case.driver).key_down(Keys.SHIFT).click(
+            element
+        ).key_up(Keys.SHIFT).perform()
+
+    def do_bulk_expand_branch_with_key(self, string: str) -> None:
+        # bulk expand by SHIFT + click.
+        element = self.test_case.find_element(
+            f'//a[contains(., "{string}")]/../*[@data-handler="collapsed"]',
+            by=By.XPATH,
+        )
+        ActionChains(self.test_case.driver).key_down(Keys.SHIFT).click(
+            element
+        ).key_up(Keys.SHIFT).perform()
+
+    def do_bulk_collapse_all(self) -> None:
+        self.test_case.click_xpath(
+            '//*[@js-collapsible_list-bulk_controls]//*[@data-bulk="collapse"]',
+        )
+
+    def do_bulk_expand_all(self) -> None:
+        self.test_case.click_xpath(
+            '//*[@js-collapsible_list-bulk_controls]//*[@data-bulk="expand"]',
+        )
+
+    def do_undo_from(self, role: str) -> None:
+        self.test_case.click_xpath(
+            "//*[@js-collapsible_list-bulk_controls]"
+            f'//*[@data-bulk="{role}"][@data-mode="undo"]'
+        )
+
+    def do_undo_from_collapse_all(self) -> None:
+        self.test_case.click_xpath(
+            "//*[@js-collapsible_list-bulk_controls]"
+            '//*[@data-bulk="collapse"][@data-mode="undo"]'
+        )
+
+    def do_undo_from_expand_all(self) -> None:
+        self.test_case.click_xpath(
+            "//*[@js-collapsible_list-bulk_controls]"
+            '//*[@data-bulk="expand"][@data-mode="undo"]'
+        )
+
+    def assert_handler_has_undo(self, role: str) -> None:
+        # role is "collapse" or "expand"
+        self.test_case.assert_element(
+            (
+                "//*[@js-collapsible_list-bulk_controls]"
+                f"//*[@data-bulk='{role}'][@data-mode='undo']"
+            ),
+            by=By.XPATH,
+        )
+
+    def assert_handler_has_not_undo(self, role: str) -> None:
+        # role is "collapse" or "expand"
+        self.test_case.assert_element(
+            (
+                "//*[@js-collapsible_list-bulk_controls]"
+                f"//*[@data-bulk='{role}'][@data-mode='bulk']"
+            ),
+            by=By.XPATH,
+        )
+        self.test_case.assert_element(
+            (
+                "//*[@js-collapsible_list-bulk_controls]"
+                f"//*[@data-bulk='{role}'][not(@data-mode='undo')]"
+            ),
             by=By.XPATH,
         )

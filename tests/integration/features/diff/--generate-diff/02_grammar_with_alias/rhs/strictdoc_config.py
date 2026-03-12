630e3225f20faf2a996eba01240f2cd9cb9462c1
Stanislav Pankevich <s.pankevich@gmail.com>
2026-03-12 21:03:05 +0100

    test(diff): add integration test for grammar with alias

---
+++ tests/integration/features/diff/--generate-diff/02_grammar_with_alias/rhs/strictdoc_config.py
@@ -0,0 +1,14 @@
+from strictdoc.core.project_config import ProjectConfig
+
+
+def create_config() -> ProjectConfig:
+    config = ProjectConfig(
+        project_title="Test project",
+        project_features=[
+            "DIFF",
+        ],
+        grammars={
+            "@my_grammar": "grammar.sgra",
+        },
+    )
+    return config

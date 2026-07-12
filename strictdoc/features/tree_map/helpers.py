c745a9664cce63d8a851b3e9759a4741854d735e
Stanislav Pankevich <s.pankevich@gmail.com>
2026-07-12 17:47:30 +0200

    fix(tree_map): fix the presentation of very long titles when zoomin in/out

--- strictdoc/features/tree_map/helpers.py
+++ strictdoc/features/tree_map/helpers.py
@@ -2,8 +2,6 @@
 @relation(SDOC-SRS-157, scope=file)
 """
 
-from typing import Optional
-
 
 def get_color(ratio: float) -> str:
     """
@@ -35,38 +33,3 @@ def get_color(ratio: float) -> str:
         b = int(yellow[2] + (green[2] - yellow[2]) * t)
 
     return f"#{r:02x}{g:02x}{b:02x}"
-
-
-def split_into_max_n_lines(
-    text: Optional[str], max_lines: int = 3, min_line_len: int = 42
-) -> str:
-    if not text:
-        return ""
-
-    total_len = len(text)
-    ideal_len = total_len // max_lines
-    lines = []
-    start = 0
-    n = len(text)
-
-    for _ in range(max_lines - 1):
-        # Stop splitting if the remaining text is short enough
-        if n - start <= min_line_len:
-            break
-
-        end = start + ideal_len
-        if end >= n:
-            break
-
-        # Move forward until the next space
-        while end < n and text[end] != " ":
-            end += 1
-
-        # Append line and skip space
-        lines.append(text[start:end].strip())
-        start = end + 1  # skip space
-
-    # Append the remaining text as the last line
-    lines.append(text[start:].strip())
-
-    return "<br>".join(lines)
